78e03a937d3def8d7da614571b60ed397f3bbb6d
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-28 17:38:31 -0500

    added Pod class

---
+++ src/main/java/com/github/ka4ok85/wca/pod/Pod.java
@@ -0,0 +1,22 @@
+package com.github.ka4ok85.wca.pod;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Pod {
+	private static String ACCESS_URL = "https://apiPOD.silverpop.com/oauth/token";
+	private static List<Integer> podList = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+	public static String getOAuthEndpoint(int podNumber) {
+		podList.contains(0);
+		if (false == isValidPodNumber(podNumber)) {
+
+		}
+
+		return ACCESS_URL.replaceAll("POD", String.valueOf(podNumber));
+	}
+
+	private static boolean isValidPodNumber(int podNumber) {
+		return podList.contains(podNumber);
+	}
+}
