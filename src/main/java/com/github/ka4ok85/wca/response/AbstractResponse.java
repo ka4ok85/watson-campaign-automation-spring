592b4a7dc730efeb4edc86bd1f927ce94fe9ba32
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-01 14:48:30 -0500

    added AbstractResponse

---
+++ src/main/java/com/github/ka4ok85/wca/response/AbstractResponse.java
@@ -0,0 +1,14 @@
+package com.github.ka4ok85.wca.response;
+
+public class AbstractResponse {
+	private String output;
+
+	public String getOutput() {
+		return output;
+	}
+
+	@Override
+	public String toString() {
+		return "AbstractResponse [output=" + output + "]";
+	}
+}
