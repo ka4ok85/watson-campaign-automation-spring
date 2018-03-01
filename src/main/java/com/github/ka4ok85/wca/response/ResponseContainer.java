c13668179d03c80c4a7b966862e37168cf8f9356
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-01 14:46:25 -0500

    added ResponseContainer class

---
+++ src/main/java/com/github/ka4ok85/wca/response/ResponseContainer.java
@@ -0,0 +1,13 @@
+package com.github.ka4ok85.wca.response;
+
+public class ResponseContainer<T> {
+	private T resposne;
+
+	public T getResposne() {
+		return resposne;
+	}
+
+	public void setResposne(T resposne) {
+		this.resposne = resposne;
+	}
+}
