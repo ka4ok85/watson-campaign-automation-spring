e0f8657a077da9cf8fd2749819129ec22a318247
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-02 15:34:36 -0500

    added FailedGetAccessTokenException

---
+++ src/main/java/com/github/ka4ok85/wca/exceptions/FailedGetAccessTokenException.java
@@ -0,0 +1,9 @@
+package com.github.ka4ok85.wca.exceptions;
+
+public class FailedGetAccessTokenException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public FailedGetAccessTokenException(String message) {
+		super(message);
+	}
+}
