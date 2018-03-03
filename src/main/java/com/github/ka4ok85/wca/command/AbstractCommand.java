dec11d72d085eb2c246c35642ef696e0ad618703
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-03 17:25:24 -0500

    added oAuthClient property

--- src/main/java/com/github/ka4ok85/wca/command/AbstractCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/AbstractCommand.java
@@ -1,15 +1,23 @@
 package com.github.ka4ok85.wca.command;
 
+import com.github.ka4ok85.wca.oauth.OAuthClient;
 import com.github.ka4ok85.wca.options.AbstractOptions;
 import com.github.ka4ok85.wca.response.AbstractResponse;
 import com.github.ka4ok85.wca.response.ResponseContainer;
 
 public class AbstractCommand<T extends AbstractResponse> {
 	protected ResponseContainer<T> response = new ResponseContainer<T>();
+	protected OAuthClient oAuthClient;
 
 	public ResponseContainer<T> executeCommand(AbstractOptions options) {
 		System.out.println("Running Command with options " + options.getClass());
 
 		return response;
 	}
+
+	public void setoAuthClient(OAuthClient oAuthClient) {
+		this.oAuthClient = oAuthClient;
+	}
+	
+	
 }
