0c7c55a325200e221962f0cd3b0df075e042364f
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-03 12:45:29 -0500

    added logging

--- src/main/java/com/github/ka4ok85/wca/oauth/OAuthClientImplementation.java
+++ src/main/java/com/github/ka4ok85/wca/oauth/OAuthClientImplementation.java
@@ -2,6 +2,8 @@ package com.github.ka4ok85.wca.oauth;
 
 import java.time.LocalDateTime;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -11,6 +13,7 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import com.github.ka4ok85.wca.Engage;
 import com.github.ka4ok85.wca.exceptions.FailedGetAccessTokenException;
 import com.github.ka4ok85.wca.pod.Pod;
 import com.github.ka4ok85.wca.response.AccessTokenResponse;
@@ -26,8 +29,9 @@ public class OAuthClientImplementation implements OAuthClient {
 	private String accessUrl;
 	private String accessToken = "";
 	private LocalDateTime accessTokenExpirationTime = LocalDateTime.MIN;
-	
-	
+
+	private static final Logger log = LoggerFactory.getLogger(Engage.class);
+
 	public OAuthClientImplementation(int podNumber, String clientId, String clientSecret, String refreshToken) {
 		this.podNumber = podNumber;
 		this.clientId = clientId;
@@ -35,11 +39,11 @@ public class OAuthClientImplementation implements OAuthClient {
 		this.refreshToken = refreshToken;
 		this.accessUrl = Pod.getOAuthEndpoint(podNumber);
 	}
-	
+
 	@Override
 	public String getAccessToken() throws FailedGetAccessTokenException {
 		if (LocalDateTime.now().compareTo(accessTokenExpirationTime) > 0) {
-System.out.println(" call refreshAccessToken");
+			log.info("Calling Refresh Access Token API. Current Access Token Expiration Time is {}", accessTokenExpirationTime);
 			refreshAccessToken();
 		}
 
@@ -47,34 +51,28 @@ System.out.println(" call refreshAccessToken");
 	}
 
 	private void refreshAccessToken() throws FailedGetAccessTokenException {
-		System.out.println("running refreshAccessToken");
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-		MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
+		MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
 		map.add("grant_type", grantType);
 		map.add("client_id", clientId);
 		map.add("client_secret", clientSecret);
 		map.add("refresh_token", refreshToken);
 
 		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
-
 		try {
 			RestTemplate restTemplate = new RestTemplate();
-			ResponseEntity<String> result = restTemplate.postForEntity(accessUrl, request , String.class );
-			
-	        System.out.println(result.getStatusCodeValue());
-	        System.out.println(result.getBody());
-	        
-	        AccessTokenResponse accessTokenResponse = new AccessTokenResponse(result.getStatusCodeValue(), result.getBody());
-	        accessTokenResponse.getAccessToken();
-	        accessTokenResponse.getAccessTokenExpirationTime();
-		} catch (HttpStatusCodeException e) {
-			throw new FailedGetAccessTokenException("Can not get Access Token");
-		} catch (FailedGetAccessTokenException e) {
+			ResponseEntity<String> result = restTemplate.postForEntity(accessUrl, request, String.class);
+
+			log.debug("Refresh Access Token API Call Result: Status Code={}, Body={}", result.getStatusCodeValue(), result.getBody());
+
+			AccessTokenResponse accessTokenResponse = new AccessTokenResponse(result.getStatusCodeValue(), result.getBody());
+			accessTokenResponse.getAccessToken();
+			accessTokenResponse.getAccessTokenExpirationTime();
+		} catch (HttpStatusCodeException | FailedGetAccessTokenException e) {
+			log.error("Refresh Access Token API Call Error: {}" + e.getMessage());
 			throw new FailedGetAccessTokenException("Can not get Access Token");
 		}
-
-
 	}
 }
