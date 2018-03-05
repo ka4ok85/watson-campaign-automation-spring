7ac5209102574e3e423316aacd9e6e72c65f59c6
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-05 18:12:27 -0500

    added DateTimeRange class

---
+++ src/main/java/com/github/ka4ok85/wca/utils/DateTimeRange.java
@@ -0,0 +1,37 @@
+package com.github.ka4ok85.wca.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class DateTimeRange {
+
+	private final LocalDateTime startDateTime;
+	private final LocalDateTime endDateTime;
+	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");
+
+	public DateTimeRange(LocalDateTime startDateTime, LocalDateTime endDateTime) {
+		super();
+		this.startDateTime = Objects.requireNonNull(startDateTime, "StartDateTime must not be null");
+		this.endDateTime = Objects.requireNonNull(endDateTime, "EndDateTime must not be null");
+
+		if (false == startDateTime.isBefore(endDateTime)) {
+			throw new RuntimeException("Start DateTime must be before End DateTime. Start DateTime is "
+					+ startDateTime.format(formatter) + ", End DateTime is " + endDateTime.format(formatter));
+		}
+	}
+
+	public LocalDateTime getStartDateTime() {
+		return startDateTime;
+	}
+
+	public LocalDateTime getEndDateTime() {
+		return endDateTime;
+	}
+
+	@Override
+	public String toString() {
+		return "DateTimeRange [startDateTime=" + startDateTime + ", endDateTime=" + endDateTime + "]";
+	}
+
+}
