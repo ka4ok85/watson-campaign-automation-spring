fc0d3d8c5f1c8ae8519059948ba29cd3479610ca
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-06 19:34:16 -0500

    started XML building piece for ExportList API

--- src/main/java/com/github/ka4ok85/wca/command/AbstractCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/AbstractCommand.java
@@ -6,7 +6,6 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -37,7 +36,6 @@ public abstract class AbstractCommand<T extends AbstractResponse, V extends Abst
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	public ResponseContainer<T> executeCommand(V options) {
@@ -49,18 +47,18 @@ public abstract class AbstractCommand<T extends AbstractResponse, V extends Abst
 	protected String getXML() {
 		DOMSource domSource = new DOMSource(doc);
 		StringWriter writer = new StringWriter();
-	    StreamResult result = new StreamResult(writer);
-	    TransformerFactory tf = TransformerFactory.newInstance();
-	    Transformer transformer;
+		StreamResult result = new StreamResult(writer);
+		TransformerFactory tf = TransformerFactory.newInstance();
+		Transformer transformer;
 		try {
 			transformer = tf.newTransformer();
-		    transformer.transform(domSource, result);
+			transformer.transform(domSource, result);
 		} catch (TransformerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-	    return writer.toString();
+		return writer.toString();
 	}
 
 	public void setoAuthClient(OAuthClient oAuthClient) {
--- src/main/java/com/github/ka4ok85/wca/command/ExportListCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/ExportListCommand.java
@@ -1,17 +1,33 @@
 package com.github.ka4ok85.wca.command;
 
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
 import com.github.ka4ok85.wca.options.ExportListOptions;
 import com.github.ka4ok85.wca.response.ResponseContainer;
 import com.github.ka4ok85.wca.response.ExportListResponse;
 
 public class ExportListCommand extends AbstractCommand<ExportListResponse, ExportListOptions> {
 
+	private static String apiMethodName = "ExportList";
+
 	@Override
 	public ResponseContainer<ExportListResponse> executeCommand(ExportListOptions options) {
 		System.out.println("Running ExportListCommand with options " + options.getClass());
+
+		Element methodElement = doc.createElement(apiMethodName);
+
+		Node bodyElement = doc.getElementsByTagName("Body").item(0);
+		bodyElement.appendChild(methodElement);
+
+		String xml = getXML();
+		System.out.println(xml);
 		ExportListResponse exportListResponse = new ExportListResponse(0, "");
 		ResponseContainer<ExportListResponse> response = new ResponseContainer<ExportListResponse>(exportListResponse);
 
+		System.out.println("End ExportListCommand");
+
 		return response;
 	}
+
 }
