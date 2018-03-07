7c943ae28d002f3ea58bb08c5e6547672e69673a
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-06 19:44:13 -0500

    added check for List ID

--- src/main/java/com/github/ka4ok85/wca/options/ExportListOptions.java
+++ src/main/java/com/github/ka4ok85/wca/options/ExportListOptions.java
@@ -1,11 +1,31 @@
 package com.github.ka4ok85.wca.options;
 
+import java.util.List;
+
+import com.github.ka4ok85.wca.constants.ExportFormat;
+import com.github.ka4ok85.wca.constants.FileEncoding;
+import com.github.ka4ok85.wca.constants.ListExportType;
+import com.github.ka4ok85.wca.utils.DateTimeRange;
+
 public class ExportListOptions extends AbstractOptions {
 
-	private Long ListId;
+	private ListExportType exportType = ListExportType.ALL;
+	private ExportFormat exportFormat = ExportFormat.CSV;
+	private FileEncoding fileEncoding = FileEncoding.UTF_8;
+	private boolean includeRecipientId = false;
+	private boolean includeListId = false;
+	private boolean includeLeadSource = false;
+	private boolean addToStoredFiles = false;
+	private DateTimeRange lastModifiedRange;
+	private List<String> exportColumns;
+	private final Long ListId;
 
 	public ExportListOptions(Long listId) {
 		super();
+		if (listId < 1) {
+			throw new RuntimeException("List ID must be greater than zero. Provided List ID = " + listId);
+		}
+
 		ListId = listId;
 	}
 
@@ -13,8 +33,85 @@ public class ExportListOptions extends AbstractOptions {
 		return ListId;
 	}
 
-	public void setListId(Long listId) {
-		ListId = listId;
+	public ListExportType getExportType() {
+		return exportType;
+	}
+
+	public void setExportType(ListExportType exportType) {
+		this.exportType = exportType;
+	}
+
+	public ExportFormat getExportFormat() {
+		return exportFormat;
+	}
+
+	public void setExportFormat(ExportFormat exportFormat) {
+		this.exportFormat = exportFormat;
+	}
+
+	public FileEncoding getFileEncoding() {
+		return fileEncoding;
+	}
+
+	public void setFileEncoding(FileEncoding fileEncoding) {
+		this.fileEncoding = fileEncoding;
+	}
+
+	public boolean isIncludeRecipientId() {
+		return includeRecipientId;
+	}
+
+	public void setIncludeRecipientId(boolean includeRecipientId) {
+		this.includeRecipientId = includeRecipientId;
+	}
+
+	public boolean isIncludeListId() {
+		return includeListId;
+	}
+
+	public void setIncludeListId(boolean includeListId) {
+		this.includeListId = includeListId;
+	}
+
+	public boolean isIncludeLeadSource() {
+		return includeLeadSource;
+	}
+
+	public void setIncludeLeadSource(boolean includeLeadSource) {
+		this.includeLeadSource = includeLeadSource;
+	}
+
+	public boolean isAddToStoredFiles() {
+		return addToStoredFiles;
+	}
+
+	public void setAddToStoredFiles(boolean addToStoredFiles) {
+		this.addToStoredFiles = addToStoredFiles;
+	}
+
+	public DateTimeRange getLastModifiedRange() {
+		return lastModifiedRange;
+	}
+
+	public void setLastModifiedRange(DateTimeRange lastModifiedRange) {
+		this.lastModifiedRange = lastModifiedRange;
+	}
+
+	public List<String> getExportColumns() {
+		return exportColumns;
+	}
+
+	public void setExportColumns(List<String> exportColumns) {
+		this.exportColumns = exportColumns;
+	}
+
+	@Override
+	public String toString() {
+		return "ExportListOptions [exportType=" + exportType + ", exportFormat=" + exportFormat + ", fileEncoding="
+				+ fileEncoding + ", includeRecipientId=" + includeRecipientId + ", includeListId=" + includeListId
+				+ ", includeLeadSource=" + includeLeadSource + ", addToStoredFiles=" + addToStoredFiles
+				+ ", lastModifiedRange=" + lastModifiedRange + ", exportColumns=" + exportColumns + ", ListId=" + ListId
+				+ "]";
 	}
 
 }
