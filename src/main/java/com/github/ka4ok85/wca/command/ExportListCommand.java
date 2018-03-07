3811d951ea4ec72f2d9088870b7769c9410bc1d5
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-07 16:24:02 -0500

    added check for null, added API method node creation

--- src/main/java/com/github/ka4ok85/wca/command/ExportListCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/ExportListCommand.java
@@ -1,13 +1,20 @@
 package com.github.ka4ok85.wca.command;
 
+import java.util.Objects;
+
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
+import com.github.ka4ok85.wca.oauth.OAuthClient;
 import com.github.ka4ok85.wca.options.ExportListOptions;
 import com.github.ka4ok85.wca.response.ResponseContainer;
 import com.github.ka4ok85.wca.response.ExportListResponse;
 
 public class ExportListCommand extends AbstractCommand<ExportListResponse, ExportListOptions> {
+
+	public ExportListCommand(OAuthClient oAuthClient) {
+		super(oAuthClient);
+	}
 
 	private static String apiMethodName = "ExportList";
 
@@ -15,9 +22,10 @@ public class ExportListCommand extends AbstractCommand<ExportListResponse, Expor
 	public ResponseContainer<ExportListResponse> executeCommand(ExportListOptions options) {
 		System.out.println("Running ExportListCommand with options " + options.getClass());
 
-		Element methodElement = doc.createElement(apiMethodName);
+		Objects.requireNonNull(options, "ExportListOptions must not be null");
 
 		Node bodyElement = doc.getElementsByTagName("Body").item(0);
+		Element methodElement = doc.createElement(apiMethodName);
 		bodyElement.appendChild(methodElement);
 
 		String xml = getXML();
