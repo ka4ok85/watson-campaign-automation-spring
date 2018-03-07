362829e48ec499d5754963ca04706dd276eeeec7
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-06 19:32:05 -0500

    added XML init piece for all API requests

--- src/main/java/com/github/ka4ok85/wca/command/AbstractCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/AbstractCommand.java
@@ -1,5 +1,20 @@
 package com.github.ka4ok85.wca.command;
 
+import java.io.StringWriter;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 import com.github.ka4ok85.wca.oauth.OAuthClient;
 import com.github.ka4ok85.wca.options.AbstractOptions;
 import com.github.ka4ok85.wca.response.AbstractResponse;
@@ -7,11 +22,45 @@ import com.github.ka4ok85.wca.response.ResponseContainer;
 
 public abstract class AbstractCommand<T extends AbstractResponse, V extends AbstractOptions> {
 	protected OAuthClient oAuthClient;
+	protected Document doc;
+
+	{
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder;
+		try {
+			docBuilder = docFactory.newDocumentBuilder();
+			this.doc = docBuilder.newDocument();
+			Element rootElement = this.doc.createElement("Envelope");
+			this.doc.appendChild(rootElement);
+			Element bodyElement = this.doc.createElement("Body");
+			rootElement.appendChild(bodyElement);
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 	public ResponseContainer<T> executeCommand(V options) {
 		System.out.println("Running Command with options " + options.getClass());
 
 		return new ResponseContainer<T>(null);
+	}
+
+	protected String getXML() {
+		DOMSource domSource = new DOMSource(doc);
+		StringWriter writer = new StringWriter();
+	    StreamResult result = new StreamResult(writer);
+	    TransformerFactory tf = TransformerFactory.newInstance();
+	    Transformer transformer;
+		try {
+			transformer = tf.newTransformer();
+		    transformer.transform(domSource, result);
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	    return writer.toString();
 	}
 
 	public void setoAuthClient(OAuthClient oAuthClient) {
