d771f26836cff155c8a2516a7b984efc958a14fa
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-09 16:00:58 -0500

    added FaultApiResult exception

---
+++ src/main/java/com/github/ka4ok85/wca/exceptions/FaultApiResult.java
@@ -0,0 +1,9 @@
+package com.github.ka4ok85.wca.exceptions;
+
+public class FaultApiResult extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public FaultApiResult(String message) {
+		super(message);
+	}
+}
