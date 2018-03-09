ffb0a4679617b8aecfb6dce85a3592d62af7e22e
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-09 14:53:42 -0500

    fixed FileEncoding constants

--- src/main/java/com/github/ka4ok85/wca/constants/FileEncoding.java
+++ src/main/java/com/github/ka4ok85/wca/constants/FileEncoding.java
@@ -1,15 +1,15 @@
 package com.github.ka4ok85.wca.constants;
 
 public enum FileEncoding {
-	UTF_8(0), ISO_8859_1(1);
+	UTF_8("utf-8"), ISO_8859_1("iso-8859-1");
 
-	private int value;
+	private String value;
 
-	private FileEncoding(int value) {
+	private FileEncoding(String value) {
 		this.value = value;
 	}
 
-	public int value() {
+	public String value() {
 		return value;
 	}
 }
