1a3b7b4d846fc3439b99dc0c4b6591c616dac6f7
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-10 20:23:35 -0500

    added JobResponse class

---
+++ src/main/java/com/github/ka4ok85/wca/response/JobResponse.java
@@ -0,0 +1,10 @@
+package com.github.ka4ok85.wca.response;
+
+public class JobResponse extends AbstractResponse {
+
+	public JobResponse(int responseCode, String output) {
+		super(responseCode, output);
+		// TODO Auto-generated constructor stub
+	}
+
+}
