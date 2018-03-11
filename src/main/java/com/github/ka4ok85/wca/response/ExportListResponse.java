831dc9ca06c26f98295348f21b375cf9a77101b9
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-10 20:30:09 -0500

    added ExportListResponse class

---
+++ src/main/java/com/github/ka4ok85/wca/response/ExportListResponse.java
@@ -0,0 +1,10 @@
+package com.github.ka4ok85.wca.response;
+
+public class ExportListResponse extends AbstractResponse {
+
+	public ExportListResponse(int responseCode, String output) {
+		super(responseCode, output);
+		// TODO Auto-generated constructor stub
+	}
+
+}
