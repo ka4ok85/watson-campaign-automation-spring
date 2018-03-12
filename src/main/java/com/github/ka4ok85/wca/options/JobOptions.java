914cd6d615b072f1f04eec239d28ba425700a1d4
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-11 20:44:23 -0400

    added check for jobId

--- src/main/java/com/github/ka4ok85/wca/options/JobOptions.java
+++ src/main/java/com/github/ka4ok85/wca/options/JobOptions.java
@@ -4,12 +4,20 @@ public class JobOptions extends AbstractOptions {
 	private int jobId;
 
 	public JobOptions(int jobId) {
-		super();
+		if (jobId < 1) {
+			throw new RuntimeException("Job ID must be greater than zero. Provided Job ID = " + jobId);
+		}
+
 		this.jobId = jobId;
 	}
 
 	public int getJobId() {
 		return jobId;
+	}
+
+	@Override
+	public String toString() {
+		return "JobOptions [jobId=" + jobId + "]";
 	}
 
 }
