8012dae819e4761c2a88d0cc7a82c07bd5bb3147
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-11 21:03:48 -0400

    added JobResponse fields

--- src/main/java/com/github/ka4ok85/wca/response/JobResponse.java
+++ src/main/java/com/github/ka4ok85/wca/response/JobResponse.java
@@ -2,9 +2,41 @@ package com.github.ka4ok85.wca.response;
 
 public class JobResponse extends AbstractResponse {
 
+	private int jobId;
+	private String jobStatus;
+	private String jobDescription;
+
 	public JobResponse(int responseCode, String output) {
 		super(responseCode, output);
-		// TODO Auto-generated constructor stub
+	}
+
+	public int getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(int jobId) {
+		this.jobId = jobId;
+	}
+
+	public String getJobStatus() {
+		return jobStatus;
+	}
+
+	public void setJobStatus(String jobStatus) {
+		this.jobStatus = jobStatus;
+	}
+
+	public String getJobDescription() {
+		return jobDescription;
+	}
+
+	public void setJobDescription(String jobDescription) {
+		this.jobDescription = jobDescription;
+	}
+
+	@Override
+	public String toString() {
+		return "JobResponse [jobId=" + jobId + ", jobStatus=" + jobStatus + ", jobDescription=" + jobDescription + "]";
 	}
 
 }
