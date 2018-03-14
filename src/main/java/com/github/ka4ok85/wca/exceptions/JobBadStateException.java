35ef0d05041a0959735b8083e171465764e296be
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-14 19:50:24 -0400

    added JobBadStateException

---
+++ src/main/java/com/github/ka4ok85/wca/exceptions/JobBadStateException.java
@@ -0,0 +1,9 @@
+package com.github.ka4ok85.wca.exceptions;
+
+public class JobBadStateException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public JobBadStateException(String message) {
+		super(message);
+	}
+}
