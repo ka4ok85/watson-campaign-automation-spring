fb4b40b41ec35a04495f390f43130c6e376ecdee
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-15 21:41:25 -0400

    added BadApiResultException

---
+++ src/main/java/com/github/ka4ok85/wca/exceptions/BadApiResultException.java
@@ -0,0 +1,9 @@
+package com.github.ka4ok85.wca.exceptions;
+
+public class BadApiResultException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public BadApiResultException(String message) {
+		super(message);
+	}
+}
