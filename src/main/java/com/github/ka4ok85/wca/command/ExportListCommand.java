b35488e80521b403cbcd9d1ae710aa60ce472ef1
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-16 17:27:50 -0400

    working export file download functionality

--- src/main/java/com/github/ka4ok85/wca/command/ExportListCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/ExportListCommand.java
@@ -1,17 +1,49 @@
 package com.github.ka4ok85.wca.command;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.FileAttribute;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
 
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
+import com.github.ka4ok85.wca.Engage;
+import com.github.ka4ok85.wca.exceptions.BadApiResultException;
 import com.github.ka4ok85.wca.exceptions.FailedGetAccessTokenException;
-import com.github.ka4ok85.wca.exceptions.FaultApiResult;
+import com.github.ka4ok85.wca.exceptions.FaultApiResultException;
+import com.github.ka4ok85.wca.exceptions.JobBadStateException;
 import com.github.ka4ok85.wca.oauth.OAuthClient;
 import com.github.ka4ok85.wca.options.ExportListOptions;
 import com.github.ka4ok85.wca.response.ResponseContainer;
 import com.github.ka4ok85.wca.utils.DateTimeRange;
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
 import com.github.ka4ok85.wca.response.ExportListResponse;
+import com.github.ka4ok85.wca.response.JobResponse;
 
 public class ExportListCommand extends AbstractCommand<ExportListResponse, ExportListOptions> {
 
@@ -22,7 +54,7 @@ public class ExportListCommand extends AbstractCommand<ExportListResponse, Expor
 	private static String apiMethodName = "ExportList";
 
 	@Override
-	public ResponseContainer<ExportListResponse> executeCommand(ExportListOptions options) throws FailedGetAccessTokenException, FaultApiResult {
+	public ResponseContainer<ExportListResponse> executeCommand(ExportListOptions options) throws FailedGetAccessTokenException, FaultApiResultException, BadApiResultException {
 		System.out.println("Running ExportListCommand with options " + options.getClass());
 
 		Objects.requireNonNull(options, "ExportListOptions must not be null");
@@ -69,12 +101,71 @@ public class ExportListCommand extends AbstractCommand<ExportListResponse, Expor
 		}
 
 		String xml = getXML();
-		System.out.println(xml);
+		//System.out.println(xml);
 
 		Node resultNode = runApi(xml);
+		
+    	XPathFactory factory = XPathFactory.newInstance();
+    	XPath xpath = factory.newXPath();
+
+		try {
+			Node jobIdNode = (Node) xpath.evaluate("JOB_ID", resultNode, XPathConstants.NODE);
+			Node filePathNode = (Node) xpath.evaluate("FILE_PATH", resultNode, XPathConstants.NODE);
+			
+			System.out.println(jobIdNode.getTextContent());
+			System.out.println(filePathNode.getTextContent());
+			
+			
+			JobResponse jobResponse = waitUntilJobIsCompleted(Integer.parseInt(jobIdNode.getTextContent()));
+			if (jobResponse.isComplete()) {
+				String filePath = filePathNode.getTextContent();
+				// access result file
+				if (options.getLocalAbsoluteFilePath() != null) {
+					System.out.println("file:" + filePath);
+					
+					String username = "oauth";
+					String password = oAuthClient.getAccessToken();
+					String hostname = "transfer0.silverpop.com";
+					int port = 22;
+					
+					Session     session     = null;
+			        Channel     channel     = null;
+			        ChannelSftp channelSftp = null;
+
+			        try{
+			            JSch jsch = new JSch();
+			            session = jsch.getSession(username,hostname,port);
+			            session.setPassword(password);
+			            java.util.Properties config = new java.util.Properties();
+			            config.put("StrictHostKeyChecking", "no");
+			            session.setConfig(config);
+			            session.connect();
+			            channel = session.openChannel("sftp");
+			            channel.connect();
+			            channelSftp = (ChannelSftp)channel;
+			            channelSftp.cd("download");
+			            channelSftp.get(filePath, options.getLocalAbsoluteFilePath());
+			            channelSftp.exit();
+			            session.disconnect();
+			        }catch(Exception ex){
+			            ex.printStackTrace();
+			        }
+				}
+
+			} else {
+				// TODO exception?
+			}
+			System.out.println(jobResponse);
+			
+
+		} catch (XPathExpressionException | JobBadStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		System.out.println(resultNode);
 
-		ExportListResponse exportListResponse = new ExportListResponse(0, "");
+		ExportListResponse exportListResponse = new ExportListResponse();
 		ResponseContainer<ExportListResponse> response = new ResponseContainer<ExportListResponse>(exportListResponse);
 
 		System.out.println("End ExportListCommand");
