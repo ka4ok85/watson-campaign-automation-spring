017b20231e97d30008558a3fb46adab58d6adf51
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-16 19:11:26 -0400

    added getSFTPHostName() helper

--- src/main/java/com/github/ka4ok85/wca/pod/Pod.java
+++ src/main/java/com/github/ka4ok85/wca/pod/Pod.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Pod {
 	private static String ACCESS_URL = "https://apiPOD.silverpop.com/oauth/token";
 	private static String XML_API_URL = "https://apiPOD.silverpop.com/XMLAPI";
+	private static String SFTP_URL = "transferPOD.silverpop.com";
 	private static List<Integer> podList = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
 
 	public static String getOAuthEndpoint(int podNumber) {
@@ -22,6 +23,14 @@ public class Pod {
 		}
 
 		return XML_API_URL.replaceAll("POD", String.valueOf(podNumber));
+	}
+
+	public static String getSFTPHostName(int podNumber) {
+		if (false == isValidPodNumber(podNumber)) {
+			throw new RuntimeException("Unsupported Pod Number");
+		}
+
+		return SFTP_URL.replaceAll("POD", String.valueOf(podNumber));
 	}
 
 	private static boolean isValidPodNumber(int podNumber) {
