73bae443f0a31817803bd859fef93f30639929c0
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-18 10:59:05 -0400

    SFTP class clean up

--- src/main/java/com/github/ka4ok85/wca/sftp/SFTP.java
+++ src/main/java/com/github/ka4ok85/wca/sftp/SFTP.java
@@ -1,22 +1,14 @@
 package com.github.ka4ok85.wca.sftp;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-
-import org.apache.el.lang.ELArithmetic;
-
+import com.github.ka4ok85.wca.exceptions.FailedGetAccessTokenException;
 import com.github.ka4ok85.wca.oauth.OAuthClient;
 import com.github.ka4ok85.wca.pod.Pod;
-import com.github.ka4ok85.wca.response.ExportListResponse;
-import com.github.ka4ok85.wca.response.ResponseContainer;
 import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import com.jcraft.jsch.SftpException;
-
-
 
 public class SFTP {
 	private static int port = 22;
@@ -28,37 +20,26 @@ public class SFTP {
 		this.oAuthClient = oAuthClient;
 	}
 
-
 	public void download(String filePath, String localAbsoluteFilePath) {
-
-			try {
-				JSch jsch = new JSch();
-				Session session = jsch.getSession(username, Pod.getSFTPHostName(this.oAuthClient.getPodNumber()), port);
-				try {
-					session.setPassword(this.oAuthClient.getAccessToken());
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					System.out.println("int ex: " + e.getMessage());
-					System.out.println("int ex s: " + e.getStackTrace());
-					e.printStackTrace();
-				}
-				java.util.Properties config = new java.util.Properties();
-				config.put("StrictHostKeyChecking", "no");
-				session.setConfig(config);
-				session.connect();
-				Channel channel = session.openChannel("sftp");
-				channel.connect();
-				ChannelSftp channelSftp = (ChannelSftp) channel;
-				channelSftp.cd("download");
-				channelSftp.get(filePath, localAbsoluteFilePath);
-				channelSftp.exit();
-				session.disconnect();
-				//return channelSftp; 
-			} catch (JSchException | SftpException e11) {
-				System.out.println("ex: " + e11.getMessage());
-				System.out.println("ex s: " + e11.getStackTrace());
-				e11.printStackTrace();
-			}
-		
+		try {
+			JSch jsch = new JSch();
+			Session session = jsch.getSession(username, Pod.getSFTPHostName(this.oAuthClient.getPodNumber()), port);
+			session.setPassword(this.oAuthClient.getAccessToken());
+			java.util.Properties config = new java.util.Properties();
+			config.put("StrictHostKeyChecking", "no");
+			session.setConfig(config);
+			session.connect();
+			Channel channel = session.openChannel("sftp");
+			channel.connect();
+			ChannelSftp channelSftp = (ChannelSftp) channel;
+			channelSftp.cd("download");
+			channelSftp.get(filePath, localAbsoluteFilePath);
+			channelSftp.exit();
+			session.disconnect();
+		} catch (JSchException | SftpException e) {
+			e.printStackTrace();
+		} catch (FailedGetAccessTokenException e) {
+			e.printStackTrace();
+		}
 	}
 }
