80ae2a94afc99e3a5d5a35c8bf5eba5a074171d9
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-18 18:42:01 -0400

    added logging

--- src/main/java/com/github/ka4ok85/wca/sftp/SFTP.java
+++ src/main/java/com/github/ka4ok85/wca/sftp/SFTP.java
@@ -1,5 +1,11 @@
 package com.github.ka4ok85.wca.sftp;
 
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.ka4ok85.wca.Engage;
 import com.github.ka4ok85.wca.exceptions.FailedGetAccessTokenException;
 import com.github.ka4ok85.wca.oauth.OAuthClient;
 import com.github.ka4ok85.wca.pod.Pod;
@@ -13,6 +19,7 @@ import com.jcraft.jsch.SftpException;
 public class SFTP {
 	private static int port = 22;
 	private static String username = "oauth";
+	private static final Logger log = LoggerFactory.getLogger(SFTP.class);
 
 	private OAuthClient oAuthClient;
 
@@ -25,20 +32,30 @@ public class SFTP {
 			JSch jsch = new JSch();
 			Session session = jsch.getSession(username, Pod.getSFTPHostName(this.oAuthClient.getPodNumber()), port);
 			session.setPassword(this.oAuthClient.getAccessToken());
-			java.util.Properties config = new java.util.Properties();
+			Properties config = new Properties();
 			config.put("StrictHostKeyChecking", "no");
 			session.setConfig(config);
+
+			log.debug("Connecting to SFTP. Hostname is {}", Pod.getSFTPHostName(this.oAuthClient.getPodNumber()));
 			session.connect();
 			Channel channel = session.openChannel("sftp");
 			channel.connect();
 			ChannelSftp channelSftp = (ChannelSftp) channel;
+
+			log.debug("Changing SFTP directory to {}", "download"); 
 			channelSftp.cd("download");
+			
+			log.debug("Downloading remote file {} into local file {}", filePath, localAbsoluteFilePath);
 			channelSftp.get(filePath, localAbsoluteFilePath);
+			
+			log.debug("Disconnecting from SFTP");
 			channelSftp.exit();
 			session.disconnect();
 		} catch (JSchException | SftpException e) {
+			log.warn("SFTP Error is {}", e.getMessage());
 			e.printStackTrace();
 		} catch (FailedGetAccessTokenException e) {
+			log.warn("Can not get Access Token for SFTP. Error is {}", e.getMessage());
 			e.printStackTrace();
 		}
 	}
