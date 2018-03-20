8cad18391ac067e92e3b757389d674ba6070fc0a
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-20 13:52:41 -0400

    added fields to ExportListResponse

--- src/main/java/com/github/ka4ok85/wca/response/ExportListResponse.java
+++ src/main/java/com/github/ka4ok85/wca/response/ExportListResponse.java
@@ -2,4 +2,19 @@ package com.github.ka4ok85.wca.response;
 
 public class ExportListResponse extends AbstractResponse {
 
+	private String remoteFileName;
+
+	public String getRemoteFileName() {
+		return remoteFileName;
+	}
+
+	public void setRemoteFileName(String remoteFileName) {
+		this.remoteFileName = remoteFileName;
+	}
+
+	@Override
+	public String toString() {
+		return "ExportListResponse [remoteFileName=" + remoteFileName + "]";
+	}
+
 }
