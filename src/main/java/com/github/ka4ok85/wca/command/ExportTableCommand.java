7f489f3529122aa1e14b09c142cd37d1190076b2
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-23 16:50:51 -0400

    added logic for Export Relational Table API

--- src/main/java/com/github/ka4ok85/wca/command/ExportTableCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/ExportTableCommand.java
@@ -1,14 +1,101 @@
 package com.github.ka4ok85.wca.command;
 
+import java.util.Objects;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
+import com.github.ka4ok85.wca.exceptions.BadApiResultException;
+import com.github.ka4ok85.wca.exceptions.EngageApiException;
+import com.github.ka4ok85.wca.exceptions.FailedGetAccessTokenException;
+import com.github.ka4ok85.wca.exceptions.FaultApiResultException;
+import com.github.ka4ok85.wca.exceptions.JobBadStateException;
 import com.github.ka4ok85.wca.options.ExportTableOptions;
 import com.github.ka4ok85.wca.response.ExportTableResponse;
+import com.github.ka4ok85.wca.response.JobResponse;
+import com.github.ka4ok85.wca.response.ResponseContainer;
+import com.github.ka4ok85.wca.utils.DateTimeRange;
 
 public class ExportTableCommand extends AbstractCommand<ExportTableResponse, ExportTableOptions> {
 
 	private static final String apiMethodName = "ExportTable";
 	private static final Logger log = LoggerFactory.getLogger(ExportTableCommand.class);
+
+	@Override
+	public ResponseContainer<ExportTableResponse> executeCommand(ExportTableOptions options)
+			throws FailedGetAccessTokenException, FaultApiResultException, BadApiResultException {
+		Objects.requireNonNull(options, "ExportTableOptions must not be null");
+
+		Element methodElement = doc.createElement(apiMethodName);
+		currentNode = addChildNode(methodElement, null);
+
+		Element tableID = doc.createElement("TABLE_ID");
+		tableID.setTextContent(options.getTableId()
+				.toString());
+		addChildNode(tableID, currentNode);
+
+		Element exportFormat = doc.createElement("EXPORT_FORMAT");
+		exportFormat.setTextContent(options.getExportFormat()
+				.value());
+		addChildNode(exportFormat, currentNode);
+
+		Element fileEncoding = doc.createElement("FILE_ENCODING");
+		fileEncoding.setTextContent(options.getFileEncoding()
+				.value());
+		addChildNode(fileEncoding, currentNode);
+
+		DateTimeRange lastModifiedRange = options.getLastModifiedRange();
+		if (lastModifiedRange != null) {
+			addParameter(currentNode, "DATE_START", lastModifiedRange.getFormattedStartDateTime());
+			addParameter(currentNode, "DATE_END", lastModifiedRange.getFormattedEndDateTime());
+		}
+
+		addBooleanParameter(methodElement, "ADD_TO_STORED_FILES", options.isAddToStoredFiles());
+
+		String xml = getXML();
+		log.debug("XML Request is {}", xml);
+		Node resultNode = runApi(xml);
+
+		XPathFactory factory = XPathFactory.newInstance();
+		XPath xpath = factory.newXPath();
+		String filePath;
+		try {
+			Node jobIdNode = (Node) xpath.evaluate("JOB_ID", resultNode, XPathConstants.NODE);
+			Node filePathNode = (Node) xpath.evaluate("FILE_PATH", resultNode, XPathConstants.NODE);
+
+			final int jobId = Integer.parseInt(jobIdNode.getTextContent());
+			log.debug("Job ID {} is being excuted", jobId);
+
+			final JobResponse jobResponse = waitUntilJobIsCompleted(jobId);
+			log.debug("Job Response is {}", jobResponse);
+			if (jobResponse.isComplete()) {
+				filePath = filePathNode.getTextContent();
+				log.debug("Generated Export File {} on SFTP", filePath);
+				if (options.getLocalAbsoluteFilePath() != null) {
+					sftp.download(filePath, options.getLocalAbsoluteFilePath());
+				}
+			} else {
+				log.error("State inconsistency for Job ID {}", jobId);
+				throw new JobBadStateException("Job ID " + jobId + " was reported as Completed, but actual State is "
+						+ jobResponse.getJobStatus());
+			}
+		} catch (XPathExpressionException | JobBadStateException e) {
+			throw new EngageApiException(e.getMessage());
+		}
+
+		ExportTableResponse exportTableResponse = new ExportTableResponse();
+		exportTableResponse.setRemoteFileName(filePath);
+		ResponseContainer<ExportTableResponse> response = new ResponseContainer<ExportTableResponse>(
+				exportTableResponse);
+
+		return response;
+	}
 
 }
