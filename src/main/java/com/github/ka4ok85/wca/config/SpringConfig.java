bc86aa45f778528c19bf5c6a5b171689dd78b783
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-23 16:51:49 -0400

    added exportTable bean to the Spring config

--- src/main/java/com/github/ka4ok85/wca/config/SpringConfig.java
+++ src/main/java/com/github/ka4ok85/wca/config/SpringConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 import com.github.ka4ok85.wca.command.ExportListCommand;
+import com.github.ka4ok85.wca.command.ExportTableCommand;
 import com.github.ka4ok85.wca.command.WaitForJobCommand;
 
 @Configuration
@@ -13,6 +14,12 @@ public class SpringConfig {
 	@Scope("prototype")
 	public ExportListCommand exportList() {
 		return new ExportListCommand();
+	}
+
+	@Bean
+	@Scope("prototype")
+	public ExportTableCommand exportTable() {
+		return new ExportTableCommand();
 	}
 
 	@Bean
