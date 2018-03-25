720a3ffbb021faa6b4e63b2091ab9e5b267a3f0b
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-25 17:00:24 -0400

    added DeleteListCommand class

---
+++ src/main/java/com/github/ka4ok85/wca/command/DeleteListCommand.java
@@ -0,0 +1,15 @@
+package com.github.ka4ok85.wca.command;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.ka4ok85.wca.options.DeleteListOptions;
+import com.github.ka4ok85.wca.response.DeleteListResponse;
+
+public class DeleteListCommand extends AbstractCommand<DeleteListResponse, DeleteListOptions> {
+
+	private static final String apiMethodName = "DeleteList";
+	private static final Logger log = LoggerFactory.getLogger(DeleteListCommand.class);
+
+
+}
