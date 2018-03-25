7b33aab02d83fe136de1dd1aea1891204c4b1cd4
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-25 19:45:23 -0400

    added DeleteListOptions class

---
+++ src/main/java/com/github/ka4ok85/wca/options/DeleteListOptions.java
@@ -0,0 +1,57 @@
+package com.github.ka4ok85.wca.options;
+
+public class DeleteListOptions extends AbstractOptions {
+
+	private Long listId;
+	private String listName;
+	private boolean keepListDetails;
+	private boolean isRecursive;
+
+	public DeleteListOptions(Long listId) {
+		super();
+		if (listId < 1) {
+			throw new RuntimeException("List ID must be greater than zero. Provided List ID = " + listId);
+		}
+		this.listId = listId;
+	}
+
+	public DeleteListOptions(String listName) {
+		super();
+		if (listName != null && !listName.trim()
+				.isEmpty()) {
+			throw new RuntimeException("List Name must be non-empty String. Provided List Name = " + listName);
+		}
+		this.listName = listName;
+	}
+
+	public boolean isKeepListDetails() {
+		return keepListDetails;
+	}
+
+	public void setKeepListDetails(boolean keepListDetails) {
+		this.keepListDetails = keepListDetails;
+	}
+
+	public boolean isRecursive() {
+		return isRecursive;
+	}
+
+	public void setRecursive(boolean isRecursive) {
+		this.isRecursive = isRecursive;
+	}
+
+	public Long getListId() {
+		return listId;
+	}
+
+	public String getListName() {
+		return listName;
+	}
+
+	@Override
+	public String toString() {
+		return "DeleteListOptions [listId=" + listId + ", listName=" + listName + ", keepListDetails=" + keepListDetails
+				+ ", isRecursive=" + isRecursive + "]";
+	}
+
+}
