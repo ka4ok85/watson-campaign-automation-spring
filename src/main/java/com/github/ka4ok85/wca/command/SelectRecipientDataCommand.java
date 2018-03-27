6fc1472a21c4a429a373af17f8d51c2de0192eba
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-27 13:29:46 -0400

    added SelectRecipientDataCommand class

---
+++ src/main/java/com/github/ka4ok85/wca/command/SelectRecipientDataCommand.java
@@ -0,0 +1,15 @@
+package com.github.ka4ok85.wca.command;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.ka4ok85.wca.options.SelectRecipientDataOptions;
+import com.github.ka4ok85.wca.response.SelectRecipientDataResponse;
+
+public class SelectRecipientDataCommand extends AbstractCommand<SelectRecipientDataResponse, SelectRecipientDataOptions> {
+
+	private static final String apiMethodName = "SelectRecipientData";
+	private static final Logger log = LoggerFactory.getLogger(SelectRecipientDataCommand.class);
+
+
+}
