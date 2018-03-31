8934055ca08d3c13a472f15ecdb9ab838b8bf88e
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-31 10:43:15 -0400

    added UpdateRecipientCommand class

---
+++ src/main/java/com/github/ka4ok85/wca/command/UpdateRecipientCommand.java
@@ -0,0 +1,16 @@
+package com.github.ka4ok85.wca.command;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.ka4ok85.wca.options.UpdateRecipientOptions;
+import com.github.ka4ok85.wca.response.UpdateRecipientResponse;
+
+public class UpdateRecipientCommand extends AbstractCommand<UpdateRecipientResponse, UpdateRecipientOptions> {
+
+	private static final String apiMethodName = "UpdateRecipient";
+	private static final Logger log = LoggerFactory.getLogger(UpdateRecipientCommand.class);
+
+
+
+}
