514c21c2a1a2ea8bfdfda06d47d2cba4f6e02615
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-03-31 10:32:43 -0400

    added DoubleOptInRecipientCommand class

---
+++ src/main/java/com/github/ka4ok85/wca/command/DoubleOptInRecipientCommand.java
@@ -0,0 +1,15 @@
+package com.github.ka4ok85.wca.command;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.ka4ok85.wca.options.DoubleOptInRecipientOptions;
+import com.github.ka4ok85.wca.response.DoubleOptInRecipientResponse;
+
+public class DoubleOptInRecipientCommand extends AbstractCommand<DoubleOptInRecipientResponse, DoubleOptInRecipientOptions> {
+
+	private static final String apiMethodName = "DoubleOptInRecipient";
+	private static final Logger log = LoggerFactory.getLogger(DoubleOptInRecipientCommand.class);
+
+
+}
