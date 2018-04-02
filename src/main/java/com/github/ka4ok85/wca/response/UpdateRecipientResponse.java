56038fc5cdb42c816ddc59a9bf729e701bf842d8
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-04-02 16:42:15 -0400

    added UpdateRecipientResponse class

---
+++ src/main/java/com/github/ka4ok85/wca/response/UpdateRecipientResponse.java
@@ -0,0 +1,29 @@
+package com.github.ka4ok85.wca.response;
+
+public class UpdateRecipientResponse extends AbstractResponse {
+	private Long recipientId;
+	private boolean visitorAssociation;
+
+	public Long getRecipientId() {
+		return recipientId;
+	}
+
+	public void setRecipientId(Long recipientId) {
+		this.recipientId = recipientId;
+	}
+
+	public boolean isVisitorAssociation() {
+		return visitorAssociation;
+	}
+
+	public void setVisitorAssociation(boolean visitorAssociation) {
+		this.visitorAssociation = visitorAssociation;
+	}
+
+	@Override
+	public String toString() {
+		return "UpdateRecipientResponse [recipientId=" + recipientId + ", visitorAssociation=" + visitorAssociation
+				+ "]";
+	}
+
+}
