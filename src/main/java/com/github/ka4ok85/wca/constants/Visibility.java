fb1df2058d8b6aace03c39599aab559c5f910fc9
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-04-08 20:21:14 -0400

    added helper for getting Visibility by integer

--- src/main/java/com/github/ka4ok85/wca/constants/Visibility.java
+++ src/main/java/com/github/ka4ok85/wca/constants/Visibility.java
@@ -12,4 +12,14 @@ public enum Visibility {
 	public Integer value() {
 		return value;
 	}
+
+	public static Visibility getVisibility(int value) {
+		for (Visibility visibility : Visibility.values()) {
+			if (visibility.value == value) {
+				return visibility;
+			}
+		}
+
+		throw new IllegalArgumentException("Visibility not found. Provided value is: " + value);
+	}
 }
