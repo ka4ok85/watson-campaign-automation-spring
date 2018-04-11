ef43edebb51e7cb6bb38265b74def87e942e5f4a
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-04-11 16:26:40 -0400

    added JoinTableCommand class

---
+++ src/main/java/com/github/ka4ok85/wca/command/JoinTableCommand.java
@@ -0,0 +1,14 @@
+package com.github.ka4ok85.wca.command;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.ka4ok85.wca.options.JoinTableOptions;
+import com.github.ka4ok85.wca.response.JoinTableResponse;
+
+public class JoinTableCommand extends AbstractCommand<JoinTableResponse, JoinTableOptions> {
+
+	private static final String apiMethodName = "JoinTable";
+	private static final Logger log = LoggerFactory.getLogger(JoinTableCommand.class);
+
+}
