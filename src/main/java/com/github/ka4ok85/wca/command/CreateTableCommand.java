5feb5d53425bccbd3547cf61251fdc0af5596e0f
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-04-12 17:22:35 -0400

    added CreateTable API logic

--- src/main/java/com/github/ka4ok85/wca/command/CreateTableCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/CreateTableCommand.java
@@ -1,14 +1,118 @@
 package com.github.ka4ok85.wca.command;
 
+import java.util.Objects;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.CDATASection;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
+import com.github.ka4ok85.wca.constants.RelationalTableColumnType;
+import com.github.ka4ok85.wca.exceptions.BadApiResultException;
+import com.github.ka4ok85.wca.exceptions.EngageApiException;
+import com.github.ka4ok85.wca.exceptions.FailedGetAccessTokenException;
+import com.github.ka4ok85.wca.exceptions.FaultApiResultException;
 import com.github.ka4ok85.wca.options.CreateTableOptions;
+import com.github.ka4ok85.wca.options.containers.RelationalTableColumn;
 import com.github.ka4ok85.wca.response.CreateTableResponse;
+import com.github.ka4ok85.wca.response.ResponseContainer;
 
+/*
+ * No way to specify Visibility. RTs is created in Shared Folder
+ */
 public class CreateTableCommand extends AbstractCommand<CreateTableResponse, CreateTableOptions> {
 
 	private static final String apiMethodName = "CreateTable";
 	private static final Logger log = LoggerFactory.getLogger(CreateTableCommand.class);
 
+	@Override
+	public ResponseContainer<CreateTableResponse> executeCommand(CreateTableOptions options)
+			throws FailedGetAccessTokenException, FaultApiResultException, BadApiResultException {
+		Objects.requireNonNull(options, "CreateTableOptions must not be null");
+
+		Element methodElement = doc.createElement(apiMethodName);
+		currentNode = addChildNode(methodElement, null);
+
+		Element tableName = doc.createElement("TABLE_NAME");
+		tableName.setTextContent(options.getTableName());
+		addChildNode(tableName, currentNode);
+
+		if (options.getColumns().size() > 0) {
+			Element columns = doc.createElement("COLUMNS");
+			addChildNode(columns, currentNode);
+			for (RelationalTableColumn tableColumn : options.getColumns()) {
+				Element column = doc.createElement("COLUMN");
+				addChildNode(column, columns);
+
+				Element columnName = doc.createElement("NAME");
+				CDATASection cdata = doc.createCDATASection(tableColumn.getName());
+				columnName.appendChild(cdata);
+				addChildNode(columnName, column);
+
+				Element columnType = doc.createElement("TYPE");
+				cdata = doc.createCDATASection(tableColumn.getType().value());
+				columnType.appendChild(cdata);
+				addChildNode(columnType, column);
+
+				if (tableColumn.getIsRequired()) {
+					Element columnIsRequired = doc.createElement("IS_REQUIRED");
+					columnIsRequired.setTextContent("true");
+					addChildNode(columnIsRequired, column);
+				}
+
+				if (tableColumn.getIsKeyColumn()) {
+					Element columnIsKeyColumn = doc.createElement("KEY_COLUMN");
+					columnIsKeyColumn.setTextContent("true");
+					addChildNode(columnIsKeyColumn, column);
+				}
+
+				if (tableColumn.getDefaultValue() != null) {
+					Element defaultValueColumn = doc.createElement("DEFAULT_VALUE");
+					cdata = doc.createCDATASection(tableColumn.getDefaultValue());
+					defaultValueColumn.appendChild(cdata);
+					addChildNode(defaultValueColumn, column);
+				}
+
+				if (tableColumn.getType().equals(RelationalTableColumnType.SELECTION)
+						&& tableColumn.getSelectionValues().size() > 0) {
+					Element selectionValues = doc.createElement("SELECTION_VALUES");
+					addChildNode(selectionValues, column);
+					for (String selection : tableColumn.getSelectionValues()) {
+						Element selectionValue = doc.createElement("VALUE");
+						cdata = doc.createCDATASection(selection);
+						selectionValue.appendChild(cdata);
+						addChildNode(selectionValue, selectionValues);
+					}
+				}
+			}
+		}
+
+		String xml = getXML();
+		log.debug("XML Request is {}", xml);
+		Node resultNode = runApi(xml);
+
+		XPathFactory factory = XPathFactory.newInstance();
+		XPath xpath = factory.newXPath();
+		Long tableId;
+		try {
+			Node tableIdNode = (Node) xpath.evaluate("TABLE_ID", resultNode, XPathConstants.NODE);
+			tableId = Long.parseLong(tableIdNode.getTextContent());
+			log.debug("Created Relational Table ID is {}", tableId);
+		} catch (XPathExpressionException e) {
+			throw new EngageApiException(e.getMessage());
+		}
+
+		CreateTableResponse createTableResponse = new CreateTableResponse();
+		createTableResponse.setTableId(tableId);
+		ResponseContainer<CreateTableResponse> response = new ResponseContainer<CreateTableResponse>(
+				createTableResponse);
+
+		return response;
+	}
 }
