9265bd79d7cbfe450815c15c73f1d1cb0b578e0f
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-04-14 17:40:57 -0400

    added container InsertUpdateRelationalTableFailure

---
+++ src/main/java/com/github/ka4ok85/wca/response/containers/InsertUpdateRelationalTableFailure.java
@@ -0,0 +1,43 @@
+package com.github.ka4ok85.wca.response.containers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class InsertUpdateRelationalTableFailure {
+
+	private String failureType;
+	private String description;
+	private List<Map<String, String>> columns = new ArrayList<Map<String, String>>();
+
+	public String getFailureType() {
+		return failureType;
+	}
+
+	public void setFailureType(String failureType) {
+		this.failureType = failureType;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Map<String, String>> getColumns() {
+		return columns;
+	}
+
+	public void setColumns(List<Map<String, String>> columns) {
+		this.columns = columns;
+	}
+
+	@Override
+	public String toString() {
+		return "InsertUpdateRelationalTableFailure [failureType=" + failureType + ", description=" + description
+				+ ", columns=" + columns + "]";
+	}
+
+}
