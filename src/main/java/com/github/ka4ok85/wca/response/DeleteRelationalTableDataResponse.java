bf49760a1b7c899a7b0d3dbb02acc2d44008b220
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-04-16 17:05:49 -0400

    added DeleteRelationalTableDataResponse class

---
+++ src/main/java/com/github/ka4ok85/wca/response/DeleteRelationalTableDataResponse.java
@@ -0,0 +1,25 @@
+package com.github.ka4ok85.wca.response;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.ka4ok85.wca.response.containers.RelationalTableRecordFailure;
+
+public class DeleteRelationalTableDataResponse extends AbstractResponse {
+
+	private List<RelationalTableRecordFailure> failures = new ArrayList<RelationalTableRecordFailure>();
+
+	public List<RelationalTableRecordFailure> getFailures() {
+		return failures;
+	}
+
+	public void setFailures(List<RelationalTableRecordFailure> failures) {
+		this.failures = failures;
+	}
+
+	@Override
+	public String toString() {
+		return "DeleteRelationalTableDataResponse [failures=" + failures + "]";
+	}
+
+}
