91120474952b210c0c2b32cb6d72069d5cb79bed
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-04-19 17:01:43 -0400

    added logic for DeleteTable API

--- src/main/java/com/github/ka4ok85/wca/command/DeleteTableCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/DeleteTableCommand.java
@@ -1,14 +1,89 @@
 package com.github.ka4ok85.wca.command;
 
+import java.util.Objects;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
+import com.github.ka4ok85.wca.exceptions.BadApiResultException;
+import com.github.ka4ok85.wca.exceptions.EngageApiException;
+import com.github.ka4ok85.wca.exceptions.FailedGetAccessTokenException;
+import com.github.ka4ok85.wca.exceptions.FaultApiResultException;
+import com.github.ka4ok85.wca.exceptions.JobBadStateException;
 import com.github.ka4ok85.wca.options.DeleteTableOptions;
 import com.github.ka4ok85.wca.response.DeleteTableResponse;
+import com.github.ka4ok85.wca.response.JobResponse;
+import com.github.ka4ok85.wca.response.ResponseContainer;
 
 public class DeleteTableCommand extends AbstractCommand<DeleteTableResponse, DeleteTableOptions> {
 
 	private static final String apiMethodName = "DeleteTable";
 	private static final Logger log = LoggerFactory.getLogger(DeleteTableCommand.class);
 
+	@Override
+	public ResponseContainer<DeleteTableResponse> executeCommand(DeleteTableOptions options)
+			throws FailedGetAccessTokenException, FaultApiResultException, BadApiResultException {
+		Objects.requireNonNull(options, "DeleteTableOptions must not be null");
+
+		Element methodElement = doc.createElement(apiMethodName);
+		currentNode = addChildNode(methodElement, null);
+
+		if (options.getTableId() != null) {
+			Element tableId = doc.createElement("TABLE_ID");
+			tableId.setTextContent(options.getTableId().toString());
+			addChildNode(tableId, currentNode);
+		} else if (options.getTableName() != null) {
+			Element tableName = doc.createElement("TABLE_NAME");
+			tableName.setTextContent(options.getTableName());
+			addChildNode(tableName, currentNode);
+		} else {
+			throw new RuntimeException("You must specify either Table ID or Table Name");
+		}
+
+		if (options.getTableVisibility() != null) {
+			Element tableVisibility = doc.createElement("TABLE_VISIBILITY");
+			tableVisibility.setTextContent(options.getTableVisibility().value().toString());
+			addChildNode(tableVisibility, currentNode);
+		}
+
+		String xml = getXML();
+		log.debug("XML Request is {}", xml);
+		Node resultNode = runApi(xml);
+
+		XPathFactory factory = XPathFactory.newInstance();
+		XPath xpath = factory.newXPath();
+		Long jobId;
+		try {
+			Node jobIdNode = (Node) xpath.evaluate("JOB_ID", resultNode, XPathConstants.NODE);
+
+			jobId = Long.parseLong(jobIdNode.getTextContent());
+			log.debug("Job ID {} is being excuted", jobId);
+
+			final JobResponse jobResponse = waitUntilJobIsCompleted(jobId);
+			log.debug("Job Response is {}", jobResponse);
+			if (jobResponse.isComplete()) {
+				log.debug("Job is completed");
+			} else {
+				log.error("State inconsistency for Job ID {}", jobId);
+				throw new JobBadStateException("Job ID " + jobId + " was reported as Completed, but actual State is "
+						+ jobResponse.getJobStatus());
+			}
+		} catch (XPathExpressionException | JobBadStateException e) {
+			throw new EngageApiException(e.getMessage());
+		}
+
+		DeleteTableResponse deleteTableResponse = new DeleteTableResponse();
+		deleteTableResponse.setJobId(jobId);
+		ResponseContainer<DeleteTableResponse> response = new ResponseContainer<DeleteTableResponse>(
+				deleteTableResponse);
+
+		return response;
+	}
 }
