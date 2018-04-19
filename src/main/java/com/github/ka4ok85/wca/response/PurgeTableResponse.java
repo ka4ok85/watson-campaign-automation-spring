8540eeceecd3017551bc5d8bd38159d1787d3afb
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-04-19 17:02:46 -0400

    added jobId to PurgeTableResponse

--- src/main/java/com/github/ka4ok85/wca/response/PurgeTableResponse.java
+++ src/main/java/com/github/ka4ok85/wca/response/PurgeTableResponse.java
@@ -1,5 +1,18 @@
 package com.github.ka4ok85.wca.response;
 
 public class PurgeTableResponse extends AbstractResponse {
+	private Long JobId;
 
+	public Long getJobId() {
+		return JobId;
+	}
+
+	public void setJobId(Long jobId) {
+		JobId = jobId;
+	}
+
+	@Override
+	public String toString() {
+		return "PurgeTableResponse [JobId=" + JobId + "]";
+	}
 }
