2f1e49acf25043490e3b63db65ca120ca5915b00
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-04-20 11:17:25 -0400

    added logic for CalculateQuery API

--- src/main/java/com/github/ka4ok85/wca/command/CalculateQueryCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/CalculateQueryCommand.java
@@ -1,15 +1,75 @@
-package com.github.ka4ok85.wca.options;
+package com.github.ka4ok85.wca.command;
+
+import java.util.Objects;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
-import com.github.ka4ok85.wca.command.AbstractCommand;
-import com.github.ka4ok85.wca.command.CalculateQueryOptions;
+import com.github.ka4ok85.wca.exceptions.BadApiResultException;
+import com.github.ka4ok85.wca.exceptions.EngageApiException;
+import com.github.ka4ok85.wca.exceptions.FailedGetAccessTokenException;
+import com.github.ka4ok85.wca.exceptions.FaultApiResultException;
+import com.github.ka4ok85.wca.exceptions.JobBadStateException;
+import com.github.ka4ok85.wca.options.CalculateQueryOptions;
 import com.github.ka4ok85.wca.response.CalculateQueryResponse;
+import com.github.ka4ok85.wca.response.JobResponse;
+import com.github.ka4ok85.wca.response.ResponseContainer;
 
 public class CalculateQueryCommand extends AbstractCommand<CalculateQueryResponse, CalculateQueryOptions> {
 
 	private static final String apiMethodName = "CalculateQuery";
 	private static final Logger log = LoggerFactory.getLogger(CalculateQueryCommand.class);
 
+	@Override
+	public ResponseContainer<CalculateQueryResponse> executeCommand(CalculateQueryOptions options)
+			throws FailedGetAccessTokenException, FaultApiResultException, BadApiResultException {
+		Objects.requireNonNull(options, "CalculateQueryOptions must not be null");
+
+		Element methodElement = doc.createElement(apiMethodName);
+		currentNode = addChildNode(methodElement, null);
+
+		Element queryId = doc.createElement("QUERY_ID");
+		queryId.setTextContent(options.getQueryId().toString());
+		addChildNode(queryId, currentNode);
+
+		String xml = getXML();
+		log.debug("XML Request is {}", xml);
+		Node resultNode = runApi(xml);
+
+		XPathFactory factory = XPathFactory.newInstance();
+		XPath xpath = factory.newXPath();
+		Long jobId;
+		try {
+			Node jobIdNode = (Node) xpath.evaluate("JOB_ID", resultNode, XPathConstants.NODE);
+
+			jobId = Long.parseLong(jobIdNode.getTextContent());
+			log.debug("Job ID {} is being excuted", jobId);
+
+			final JobResponse jobResponse = waitUntilJobIsCompleted(jobId);
+			log.debug("Job Response is {}", jobResponse);
+			if (jobResponse.isComplete()) {
+				log.debug("Job is completed");
+			} else {
+				log.error("State inconsistency for Job ID {}", jobId);
+				throw new JobBadStateException("Job ID " + jobId + " was reported as Completed, but actual State is "
+						+ jobResponse.getJobStatus());
+			}
+		} catch (XPathExpressionException | JobBadStateException e) {
+			throw new EngageApiException(e.getMessage());
+		}
+
+		CalculateQueryResponse calculateQueryResponse = new CalculateQueryResponse();
+		calculateQueryResponse.setJobId(jobId);
+		ResponseContainer<CalculateQueryResponse> response = new ResponseContainer<CalculateQueryResponse>(
+				calculateQueryResponse);
+
+		return response;
+	}
 }
