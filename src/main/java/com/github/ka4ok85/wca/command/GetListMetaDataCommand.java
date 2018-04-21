1da939cfa237b6ae58a3d8298a032969f2ae144d
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-04-21 15:09:59 -0400

    added GetListMetaDataCommand class

---
+++ src/main/java/com/github/ka4ok85/wca/command/GetListMetaDataCommand.java
@@ -0,0 +1,15 @@
+package com.github.ka4ok85.wca.command;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.ka4ok85.wca.options.GetListMetaDataOptions;
+import com.github.ka4ok85.wca.response.GetListMetaDataResponse;
+
+public class GetListMetaDataCommand extends AbstractCommand<GetListMetaDataResponse, GetListMetaDataOptions> {
+
+	private static final String apiMethodName = "GetListMetaData";
+	private static final Logger log = LoggerFactory.getLogger(GetListMetaDataCommand.class);
+
+
+}
