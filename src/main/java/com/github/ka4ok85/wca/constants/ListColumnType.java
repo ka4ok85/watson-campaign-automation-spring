45bb83b2d90e3a6be06b22b2198d7c857899dd2a
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-04-22 14:01:36 -0400

    added ListColumnType enum constants

---
+++ src/main/java/com/github/ka4ok85/wca/constants/ListColumnType.java
@@ -0,0 +1,17 @@
+package com.github.ka4ok85.wca.constants;
+
+public enum ListColumnType {
+	TEXT(0), YESNO(1), NUMERIC(2), DATE(3), TIME(4), COUNTRY(5), SELECTION(6), SEGMENTING(8), SYSTEM(9), SMS_OPT_IN(
+			13), SMS_OPTED_OUT_DATE(14), SMS_PHONE_NUMBER(15), PHONE_NUMBER(16), TIMESTAMP(17), MULTI_SELECT(20);
+
+	private int value;
+
+	private ListColumnType(Integer value) {
+		this.value = value;
+	}
+
+	public Integer value() {
+		return value;
+	}
+
+}
