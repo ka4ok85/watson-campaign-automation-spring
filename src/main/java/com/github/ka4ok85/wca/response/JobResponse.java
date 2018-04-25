b6396be1532374ef849777ec9587f0fce3a7d83c
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-04-25 18:10:30 -0400

    added parameters values to JobResponse container

--- src/main/java/com/github/ka4ok85/wca/response/JobResponse.java
+++ src/main/java/com/github/ka4ok85/wca/response/JobResponse.java
@@ -1,5 +1,8 @@
 package com.github.ka4ok85.wca.response;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.github.ka4ok85.wca.constants.JobStatus;
 
 public class JobResponse extends AbstractResponse {
@@ -7,6 +10,7 @@ public class JobResponse extends AbstractResponse {
 	private Long jobId;
 	private JobStatus jobStatus;
 	private String jobDescription;
+	private Map<String, String> parameters = new HashMap<String, String>();
 
 	public JobResponse() {
 
@@ -36,9 +40,18 @@ public class JobResponse extends AbstractResponse {
 		this.jobDescription = jobDescription;
 	}
 
+	public Map<String, String> getParameters() {
+		return parameters;
+	}
+
+	public void setParameters(Map<String, String> parameters) {
+		this.parameters = parameters;
+	}
+
 	@Override
 	public String toString() {
-		return "JobResponse [jobId=" + jobId + ", jobStatus=" + jobStatus + ", jobDescription=" + jobDescription + "]";
+		return "JobResponse [jobId=" + jobId + ", jobStatus=" + jobStatus + ", jobDescription=" + jobDescription
+				+ ", parameters=" + parameters + "]";
 	}
 
 	public boolean isRunning() {
