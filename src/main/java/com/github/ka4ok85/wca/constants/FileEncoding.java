236bff12f6b1cbddd3e4c1937d44b6702e4acd80
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-04-25 18:23:52 -0400

    added lookup method for FileEncoding enum

--- src/main/java/com/github/ka4ok85/wca/constants/FileEncoding.java
+++ src/main/java/com/github/ka4ok85/wca/constants/FileEncoding.java
@@ -12,4 +12,14 @@ public enum FileEncoding {
 	public String value() {
 		return value;
 	}
+	
+	public static FileEncoding getFileEncoding(String value) {
+		for (FileEncoding fileEncoding : FileEncoding.values()) {
+			if (fileEncoding.value.equals(value)) {
+				return fileEncoding;
+			}
+		}
+
+		throw new IllegalArgumentException("File Encoding not found. Provided value is: " + value);
+	}
 }
