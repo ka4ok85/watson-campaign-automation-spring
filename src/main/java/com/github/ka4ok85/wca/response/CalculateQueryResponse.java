eeaba8dcfe2e9f0f9d53b54927054a833c921fe8
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-04-29 15:20:09 -0400

    added more fields to CalculateQueryResponse

--- src/main/java/com/github/ka4ok85/wca/response/CalculateQueryResponse.java
+++ src/main/java/com/github/ka4ok85/wca/response/CalculateQueryResponse.java
@@ -7,6 +7,13 @@ import org.springframework.stereotype.Component;
 @Scope("prototype")
 public class CalculateQueryResponse extends AbstractResponse {
 	private Long JobId;
+	private String description;
+	private String listName;
+	private Long listSize;
+	private Long parentListId;
+	private String parentListName;
+	private String parentListType;
+	private Long queryExpressionId;
 
 	public Long getJobId() {
 		return JobId;
@@ -16,9 +23,67 @@ public class CalculateQueryResponse extends AbstractResponse {
 		JobId = jobId;
 	}
 
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getListName() {
+		return listName;
+	}
+
+	public void setListName(String listName) {
+		this.listName = listName;
+	}
+
+	public Long getListSize() {
+		return listSize;
+	}
+
+	public void setListSize(Long listSize) {
+		this.listSize = listSize;
+	}
+
+	public Long getParentListId() {
+		return parentListId;
+	}
+
+	public void setParentListId(Long parentListId) {
+		this.parentListId = parentListId;
+	}
+
+	public String getParentListName() {
+		return parentListName;
+	}
+
+	public void setParentListName(String parentListName) {
+		this.parentListName = parentListName;
+	}
+
+	public String getParentListType() {
+		return parentListType;
+	}
+
+	public void setParentListType(String parentListType) {
+		this.parentListType = parentListType;
+	}
+
+	public Long getQueryExpressionId() {
+		return queryExpressionId;
+	}
+
+	public void setQueryExpressionId(Long queryExpressionId) {
+		this.queryExpressionId = queryExpressionId;
+	}
+
 	@Override
 	public String toString() {
-		return "CalculateQueryResponse [JobId=" + JobId + "]";
+		return "CalculateQueryResponse [JobId=" + JobId + ", description=" + description + ", listName=" + listName
+				+ ", listSize=" + listSize + ", parentListId=" + parentListId + ", parentListName=" + parentListName
+				+ ", parentListType=" + parentListType + ", queryExpressionId=" + queryExpressionId + "]";
 	}
 
 }
