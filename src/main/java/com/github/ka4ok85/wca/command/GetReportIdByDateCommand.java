d8e7749a2bf834c9554db8b3f389319e52d394fa
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-05-02 15:19:42 -0400

    added logic for GetReportIdByDate API implementation

--- src/main/java/com/github/ka4ok85/wca/command/GetReportIdByDateCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/GetReportIdByDateCommand.java
@@ -1,11 +1,34 @@
 package com.github.ka4ok85.wca.command;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import com.github.ka4ok85.wca.exceptions.BadApiResultException;
+import com.github.ka4ok85.wca.exceptions.EngageApiException;
+import com.github.ka4ok85.wca.exceptions.FailedGetAccessTokenException;
+import com.github.ka4ok85.wca.exceptions.FaultApiResultException;
+import com.github.ka4ok85.wca.exceptions.JobBadStateException;
 import com.github.ka4ok85.wca.options.GetReportIdByDateOptions;
 import com.github.ka4ok85.wca.response.GetReportIdByDateResponse;
 import com.github.ka4ok85.wca.response.ResponseContainer;
+import com.github.ka4ok85.wca.response.containers.ReportIdByDateMailing;
 
 @Service
 @Scope("prototype")
@@ -14,4 +37,65 @@ public class GetReportIdByDateCommand extends AbstractCommand<GetReportIdByDateR
 	private static final String apiMethodName = "GetReportIdByDate";
 	private static final Logger log = LoggerFactory.getLogger(GetReportIdByDateCommand.class);
 
+	@Autowired
+	private GetReportIdByDateResponse getReportIdByDateResponse;
+
+	@Override
+	public ResponseContainer<GetReportIdByDateResponse> executeCommand(GetReportIdByDateOptions options)
+			throws FailedGetAccessTokenException, FaultApiResultException, BadApiResultException {
+		Objects.requireNonNull(options, "GetReportIdByDateOptions must not be null");
+
+		Element methodElement = doc.createElement(apiMethodName);
+		currentNode = addChildNode(methodElement, null);
+
+		Element mailingId = doc.createElement("MAILING_ID");
+		mailingId.setTextContent(options.getMailingId().toString());
+		addChildNode(mailingId, currentNode);
+
+		final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");
+
+		Element dateStart = doc.createElement("DATE_START");
+		dateStart.setTextContent(options.getDateStart().format(formatter));
+		addChildNode(dateStart, currentNode);
+
+		Element dateEnd = doc.createElement("DATE_END");
+		dateEnd.setTextContent(options.getDateEnd().format(formatter));
+		addChildNode(dateEnd, currentNode);
+
+		String xml = getXML();
+		log.debug("XML Request is {}", xml);
+		Node resultNode = runApi(xml);
+
+		XPathFactory factory = XPathFactory.newInstance();
+		XPath xpath = factory.newXPath();
+
+		List<ReportIdByDateMailing> mailings = new ArrayList<ReportIdByDateMailing>();
+		final DateTimeFormatter responseFormatter = DateTimeFormatter.ofPattern("M/d/yy K:mm a");
+		try {
+
+			NodeList mailingsNode = (NodeList) xpath.evaluate("Mailing", resultNode, XPathConstants.NODESET);
+			Node mailingNode;
+
+			for (int i = 0; i < mailingsNode.getLength(); i++) {
+				ReportIdByDateMailing mailingResponse = new ReportIdByDateMailing();
+				mailingNode = mailingsNode.item(i);
+				mailingResponse.setReportId(Long.parseLong(
+						((Node) xpath.evaluate("ReportId", mailingNode, XPathConstants.NODE)).getTextContent()));
+				mailingResponse.setSentDateTime(LocalDateTime.parse(
+						((Node) xpath.evaluate("SentTS", mailingNode, XPathConstants.NODE)).getTextContent(),
+						responseFormatter));
+
+				mailings.add(mailingResponse);
+			}
+		} catch (XPathExpressionException | JobBadStateException e) {
+			throw new EngageApiException(e.getMessage());
+		}
+
+		getReportIdByDateResponse.setMailings(mailings);
+
+		ResponseContainer<GetReportIdByDateResponse> response = new ResponseContainer<GetReportIdByDateResponse>(
+				getReportIdByDateResponse);
+
+		return response;
+	}
 }
