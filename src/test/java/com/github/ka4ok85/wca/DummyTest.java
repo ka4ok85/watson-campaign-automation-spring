6edefc80cb3c1dafeee3751daf33628a127c2b9b
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-05-09 15:17:23 -0400

    added dummy test

---
+++ src/test/java/com/github/ka4ok85/wca/DummyTest.java
@@ -0,0 +1,14 @@
+package com.github.ka4ok85.wca;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+	@Test
+	public void dummyTest() {
+		assertTrue(true);
+	}
+	
+}
