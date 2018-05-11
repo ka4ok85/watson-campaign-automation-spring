ded6f45c72b5ed09ba54ee043a5714d1fe5282f0
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-05-11 16:29:37 -0400

    added GetSentMailingsForListResponse class

---
+++ src/main/java/com/github/ka4ok85/wca/response/GetSentMailingsForListResponse.java
@@ -0,0 +1,39 @@
+package com.github.ka4ok85.wca.response;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.github.ka4ok85.wca.response.containers.SentMailing;
+
+@Component
+@Scope("prototype")
+public class GetSentMailingsForListResponse {
+	private Long sentMailingsCount;
+	private List<SentMailing> sentMailings = new ArrayList<SentMailing>();
+
+	public Long getSentMailingsCount() {
+		return sentMailingsCount;
+	}
+
+	public void setSentMailingsCount(Long sentMailingsCount) {
+		this.sentMailingsCount = sentMailingsCount;
+	}
+
+	public List<SentMailing> getSentMailings() {
+		return sentMailings;
+	}
+
+	public void setSentMailings(List<SentMailing> sentMailings) {
+		this.sentMailings = sentMailings;
+	}
+
+	@Override
+	public String toString() {
+		return "GetSentMailingsForListResponse [sentMailingsCount=" + sentMailingsCount + ", sentMailings="
+				+ sentMailings + "]";
+	}
+
+}
