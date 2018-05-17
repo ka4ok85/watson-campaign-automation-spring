d32af7a04af982b7895190bb0394a33d90a27239
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-05-16 20:23:06 -0400

    added more fields to ExportTableResponse

--- src/main/java/com/github/ka4ok85/wca/response/ExportTableResponse.java
+++ src/main/java/com/github/ka4ok85/wca/response/ExportTableResponse.java
@@ -6,8 +6,28 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("prototype")
 public class ExportTableResponse extends AbstractResponse {
-
+	private String description;
+	private String remoteFilePath;
 	private String remoteFileName;
+	private String listName;
+	private Long listId;
+	private Long numProcessed;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getRemoteFilePath() {
+		return remoteFilePath;
+	}
+
+	public void setRemoteFilePath(String remoteFilePath) {
+		this.remoteFilePath = remoteFilePath;
+	}
 
 	public String getRemoteFileName() {
 		return remoteFileName;
@@ -17,9 +37,35 @@ public class ExportTableResponse extends AbstractResponse {
 		this.remoteFileName = remoteFileName;
 	}
 
+	public String getListName() {
+		return listName;
+	}
+
+	public void setListName(String listName) {
+		this.listName = listName;
+	}
+
+	public Long getListId() {
+		return listId;
+	}
+
+	public void setListId(Long listId) {
+		this.listId = listId;
+	}
+
+	public Long getNumProcessed() {
+		return numProcessed;
+	}
+
+	public void setNumProcessed(Long numProcessed) {
+		this.numProcessed = numProcessed;
+	}
+
 	@Override
 	public String toString() {
-		return "ExportTableResponse [remoteFileName=" + remoteFileName + "]";
+		return "ExportTableResponse [description=" + description + ", remoteFilePath=" + remoteFilePath
+				+ ", remoteFileName=" + remoteFileName + ", listName=" + listName + ", listId=" + listId
+				+ ", numProcessed=" + numProcessed + "]";
 	}
 
 }
