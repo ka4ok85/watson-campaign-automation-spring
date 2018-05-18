006d3d4130aba98cba0cf827adbc8bca4d2ebec1
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-05-18 14:10:02 -0400

    added AggregateTrackingDataMailing class

---
+++ src/main/java/com/github/ka4ok85/wca/response/containers/AggregateTrackingDataMailing.java
@@ -0,0 +1,513 @@
+package com.github.ka4ok85.wca.response.containers;
+
+import java.time.LocalDateTime;
+
+public class AggregateTrackingDataMailing {
+	private Long mailingId;
+	private Long reportId;
+	private String mailingName;
+	private LocalDateTime SentDateTime;
+	private Long numSent;
+	private Long numSeeds;
+	private Long numSuppressed;
+	private Long numInboxMonitored;
+	private Long numBounceHard;
+	private Long numBounceSoft;
+	private Long numUniqueOpen;
+	private Long numGrossOpen;
+	private Long numUniqueClick;
+	private Long numGrossClick;
+	private Long numUniqueAttach;
+	private Long numGrossAttach;
+	private Long numUniqueClickstreams;
+	private Long numGrossClickstreams;
+	private Long numUniqueMedia;
+	private Long numGrossMedia;
+	private Long numGrossAbuse;
+	private Long numGrossChangeAddress;
+	private Long numGrossMailBlock;
+	private Long numGrossMailRestriction;
+	private Long numGrossOther;
+	private Long numConversions;
+	private Long numConversionAmount;
+	private Long numBounceHardFwd;
+	private Long numBounceSoftFwd;
+	private Long numConversionAmountFwd;
+	private Long numAttachOpenFwd;
+	private Long numClickFwd;
+	private Long numUniqueForwardFwd;
+	private Long numGrossForwardFwd;
+	private Long numUniqueConversionsFwd;
+	private Long numGrossConversionsFwd;
+	private Long numUniqueClickstreamFwd;
+	private Long numGrossClickstreamFwd;
+	private Long numUniqueClickFwd;
+	private Long numGrossClickFwd;
+	private Long numUniqueAttachOpenFwd;
+	private Long numGrossAttachOpenFwd;
+	private Long numUniqueMediaFwd;
+	private Long numGrossMediaFwd;
+	private Long numUniqueOpenFwd;
+	private Long numGrossOpenFwd;
+	private Long numAbuseFwd;
+	private Long numChangeAddressFwd;
+	private Long numMailRestrictionFwd;
+	private Long numMailBlockFwd;
+	private Long numOtherFwd;
+	private Long numSuppressedFwd;
+	private Long numUnsubscribes;
+
+	public Long getMailingId() {
+		return mailingId;
+	}
+
+	public void setMailingId(Long mailingId) {
+		this.mailingId = mailingId;
+	}
+
+	public Long getReportId() {
+		return reportId;
+	}
+
+	public void setReportId(Long reportId) {
+		this.reportId = reportId;
+	}
+
+	public String getMailingName() {
+		return mailingName;
+	}
+
+	public void setMailingName(String mailingName) {
+		this.mailingName = mailingName;
+	}
+
+	public LocalDateTime getSentDateTime() {
+		return SentDateTime;
+	}
+
+	public void setSentDateTime(LocalDateTime sentDateTime) {
+		SentDateTime = sentDateTime;
+	}
+
+	public Long getNumSent() {
+		return numSent;
+	}
+
+	public void setNumSent(Long numSent) {
+		this.numSent = numSent;
+	}
+
+	public Long getNumSeeds() {
+		return numSeeds;
+	}
+
+	public void setNumSeeds(Long numSeeds) {
+		this.numSeeds = numSeeds;
+	}
+
+	public Long getNumSuppressed() {
+		return numSuppressed;
+	}
+
+	public void setNumSuppressed(Long numSuppressed) {
+		this.numSuppressed = numSuppressed;
+	}
+
+	public Long getNumInboxMonitored() {
+		return numInboxMonitored;
+	}
+
+	public void setNumInboxMonitored(Long numInboxMonitored) {
+		this.numInboxMonitored = numInboxMonitored;
+	}
+
+	public Long getNumBounceHard() {
+		return numBounceHard;
+	}
+
+	public void setNumBounceHard(Long numBounceHard) {
+		this.numBounceHard = numBounceHard;
+	}
+
+	public Long getNumBounceSoft() {
+		return numBounceSoft;
+	}
+
+	public void setNumBounceSoft(Long numBounceSoft) {
+		this.numBounceSoft = numBounceSoft;
+	}
+
+	public Long getNumUniqueOpen() {
+		return numUniqueOpen;
+	}
+
+	public void setNumUniqueOpen(Long numUniqueOpen) {
+		this.numUniqueOpen = numUniqueOpen;
+	}
+
+	public Long getNumGrossOpen() {
+		return numGrossOpen;
+	}
+
+	public void setNumGrossOpen(Long numGrossOpen) {
+		this.numGrossOpen = numGrossOpen;
+	}
+
+	public Long getNumUniqueClick() {
+		return numUniqueClick;
+	}
+
+	public void setNumUniqueClick(Long numUniqueClick) {
+		this.numUniqueClick = numUniqueClick;
+	}
+
+	public Long getNumGrossClick() {
+		return numGrossClick;
+	}
+
+	public void setNumGrossClick(Long numGrossClick) {
+		this.numGrossClick = numGrossClick;
+	}
+
+	public Long getNumUniqueAttach() {
+		return numUniqueAttach;
+	}
+
+	public void setNumUniqueAttach(Long numUniqueAttach) {
+		this.numUniqueAttach = numUniqueAttach;
+	}
+
+	public Long getNumGrossAttach() {
+		return numGrossAttach;
+	}
+
+	public void setNumGrossAttach(Long numGrossAttach) {
+		this.numGrossAttach = numGrossAttach;
+	}
+
+	public Long getNumUniqueClickstreams() {
+		return numUniqueClickstreams;
+	}
+
+	public void setNumUniqueClickstreams(Long numUniqueClickstreams) {
+		this.numUniqueClickstreams = numUniqueClickstreams;
+	}
+
+	public Long getNumGrossClickstreams() {
+		return numGrossClickstreams;
+	}
+
+	public void setNumGrossClickstreams(Long numGrossClickstreams) {
+		this.numGrossClickstreams = numGrossClickstreams;
+	}
+
+	public Long getNumUniqueMedia() {
+		return numUniqueMedia;
+	}
+
+	public void setNumUniqueMedia(Long numUniqueMedia) {
+		this.numUniqueMedia = numUniqueMedia;
+	}
+
+	public Long getNumGrossMedia() {
+		return numGrossMedia;
+	}
+
+	public void setNumGrossMedia(Long numGrossMedia) {
+		this.numGrossMedia = numGrossMedia;
+	}
+
+	public Long getNumGrossAbuse() {
+		return numGrossAbuse;
+	}
+
+	public void setNumGrossAbuse(Long numGrossAbuse) {
+		this.numGrossAbuse = numGrossAbuse;
+	}
+
+	public Long getNumGrossChangeAddress() {
+		return numGrossChangeAddress;
+	}
+
+	public void setNumGrossChangeAddress(Long numGrossChangeAddress) {
+		this.numGrossChangeAddress = numGrossChangeAddress;
+	}
+
+	public Long getNumGrossMailBlock() {
+		return numGrossMailBlock;
+	}
+
+	public void setNumGrossMailBlock(Long numGrossMailBlock) {
+		this.numGrossMailBlock = numGrossMailBlock;
+	}
+
+	public Long getNumGrossMailRestriction() {
+		return numGrossMailRestriction;
+	}
+
+	public void setNumGrossMailRestriction(Long numGrossMailRestriction) {
+		this.numGrossMailRestriction = numGrossMailRestriction;
+	}
+
+	public Long getNumGrossOther() {
+		return numGrossOther;
+	}
+
+	public void setNumGrossOther(Long numGrossOther) {
+		this.numGrossOther = numGrossOther;
+	}
+
+	public Long getNumConversions() {
+		return numConversions;
+	}
+
+	public void setNumConversions(Long numConversions) {
+		this.numConversions = numConversions;
+	}
+
+	public Long getNumConversionAmount() {
+		return numConversionAmount;
+	}
+
+	public void setNumConversionAmount(Long numConversionAmount) {
+		this.numConversionAmount = numConversionAmount;
+	}
+
+	public Long getNumBounceHardFwd() {
+		return numBounceHardFwd;
+	}
+
+	public void setNumBounceHardFwd(Long numBounceHardFwd) {
+		this.numBounceHardFwd = numBounceHardFwd;
+	}
+
+	public Long getNumBounceSoftFwd() {
+		return numBounceSoftFwd;
+	}
+
+	public void setNumBounceSoftFwd(Long numBounceSoftFwd) {
+		this.numBounceSoftFwd = numBounceSoftFwd;
+	}
+
+	public Long getNumConversionAmountFwd() {
+		return numConversionAmountFwd;
+	}
+
+	public void setNumConversionAmountFwd(Long numConversionAmountFwd) {
+		this.numConversionAmountFwd = numConversionAmountFwd;
+	}
+
+	public Long getNumAttachOpenFwd() {
+		return numAttachOpenFwd;
+	}
+
+	public void setNumAttachOpenFwd(Long numAttachOpenFwd) {
+		this.numAttachOpenFwd = numAttachOpenFwd;
+	}
+
+	public Long getNumClickFwd() {
+		return numClickFwd;
+	}
+
+	public void setNumClickFwd(Long numClickFwd) {
+		this.numClickFwd = numClickFwd;
+	}
+
+	public Long getNumUniqueForwardFwd() {
+		return numUniqueForwardFwd;
+	}
+
+	public void setNumUniqueForwardFwd(Long numUniqueForwardFwd) {
+		this.numUniqueForwardFwd = numUniqueForwardFwd;
+	}
+
+	public Long getNumGrossForwardFwd() {
+		return numGrossForwardFwd;
+	}
+
+	public void setNumGrossForwardFwd(Long numGrossForwardFwd) {
+		this.numGrossForwardFwd = numGrossForwardFwd;
+	}
+
+	public Long getNumUniqueConversionsFwd() {
+		return numUniqueConversionsFwd;
+	}
+
+	public void setNumUniqueConversionsFwd(Long numUniqueConversionsFwd) {
+		this.numUniqueConversionsFwd = numUniqueConversionsFwd;
+	}
+
+	public Long getNumGrossConversionsFwd() {
+		return numGrossConversionsFwd;
+	}
+
+	public void setNumGrossConversionsFwd(Long numGrossConversionsFwd) {
+		this.numGrossConversionsFwd = numGrossConversionsFwd;
+	}
+
+	public Long getNumUniqueClickstreamFwd() {
+		return numUniqueClickstreamFwd;
+	}
+
+	public void setNumUniqueClickstreamFwd(Long numUniqueClickstreamFwd) {
+		this.numUniqueClickstreamFwd = numUniqueClickstreamFwd;
+	}
+
+	public Long getNumGrossClickstreamFwd() {
+		return numGrossClickstreamFwd;
+	}
+
+	public void setNumGrossClickstreamFwd(Long numGrossClickstreamFwd) {
+		this.numGrossClickstreamFwd = numGrossClickstreamFwd;
+	}
+
+	public Long getNumUniqueClickFwd() {
+		return numUniqueClickFwd;
+	}
+
+	public void setNumUniqueClickFwd(Long numUniqueClickFwd) {
+		this.numUniqueClickFwd = numUniqueClickFwd;
+	}
+
+	public Long getNumGrossClickFwd() {
+		return numGrossClickFwd;
+	}
+
+	public void setNumGrossClickFwd(Long numGrossClickFwd) {
+		this.numGrossClickFwd = numGrossClickFwd;
+	}
+
+	public Long getNumUniqueAttachOpenFwd() {
+		return numUniqueAttachOpenFwd;
+	}
+
+	public void setNumUniqueAttachOpenFwd(Long numUniqueAttachOpenFwd) {
+		this.numUniqueAttachOpenFwd = numUniqueAttachOpenFwd;
+	}
+
+	public Long getNumGrossAttachOpenFwd() {
+		return numGrossAttachOpenFwd;
+	}
+
+	public void setNumGrossAttachOpenFwd(Long numGrossAttachOpenFwd) {
+		this.numGrossAttachOpenFwd = numGrossAttachOpenFwd;
+	}
+
+	public Long getNumUniqueMediaFwd() {
+		return numUniqueMediaFwd;
+	}
+
+	public void setNumUniqueMediaFwd(Long numUniqueMediaFwd) {
+		this.numUniqueMediaFwd = numUniqueMediaFwd;
+	}
+
+	public Long getNumGrossMediaFwd() {
+		return numGrossMediaFwd;
+	}
+
+	public void setNumGrossMediaFwd(Long numGrossMediaFwd) {
+		this.numGrossMediaFwd = numGrossMediaFwd;
+	}
+
+	public Long getNumUniqueOpenFwd() {
+		return numUniqueOpenFwd;
+	}
+
+	public void setNumUniqueOpenFwd(Long numUniqueOpenFwd) {
+		this.numUniqueOpenFwd = numUniqueOpenFwd;
+	}
+
+	public Long getNumGrossOpenFwd() {
+		return numGrossOpenFwd;
+	}
+
+	public void setNumGrossOpenFwd(Long numGrossOpenFwd) {
+		this.numGrossOpenFwd = numGrossOpenFwd;
+	}
+
+	public Long getNumAbuseFwd() {
+		return numAbuseFwd;
+	}
+
+	public void setNumAbuseFwd(Long numAbuseFwd) {
+		this.numAbuseFwd = numAbuseFwd;
+	}
+
+	public Long getNumChangeAddressFwd() {
+		return numChangeAddressFwd;
+	}
+
+	public void setNumChangeAddressFwd(Long numChangeAddressFwd) {
+		this.numChangeAddressFwd = numChangeAddressFwd;
+	}
+
+	public Long getNumMailRestrictionFwd() {
+		return numMailRestrictionFwd;
+	}
+
+	public void setNumMailRestrictionFwd(Long numMailRestrictionFwd) {
+		this.numMailRestrictionFwd = numMailRestrictionFwd;
+	}
+
+	public Long getNumMailBlockFwd() {
+		return numMailBlockFwd;
+	}
+
+	public void setNumMailBlockFwd(Long numMailBlockFwd) {
+		this.numMailBlockFwd = numMailBlockFwd;
+	}
+
+	public Long getNumOtherFwd() {
+		return numOtherFwd;
+	}
+
+	public void setNumOtherFwd(Long numOtherFwd) {
+		this.numOtherFwd = numOtherFwd;
+	}
+
+	public Long getNumSuppressedFwd() {
+		return numSuppressedFwd;
+	}
+
+	public void setNumSuppressedFwd(Long numSuppressedFwd) {
+		this.numSuppressedFwd = numSuppressedFwd;
+	}
+
+	public Long getNumUnsubscribes() {
+		return numUnsubscribes;
+	}
+
+	public void setNumUnsubscribes(Long numUnsubscribes) {
+		this.numUnsubscribes = numUnsubscribes;
+	}
+
+	@Override
+	public String toString() {
+		return "AggregateTrackingDataMailing [mailingId=" + mailingId + ", reportId=" + reportId + ", mailingName="
+				+ mailingName + ", SentDateTime=" + SentDateTime + ", numSent=" + numSent + ", numSeeds=" + numSeeds
+				+ ", numSuppressed=" + numSuppressed + ", numInboxMonitored=" + numInboxMonitored + ", numBounceHard="
+				+ numBounceHard + ", numBounceSoft=" + numBounceSoft + ", numUniqueOpen=" + numUniqueOpen
+				+ ", numGrossOpen=" + numGrossOpen + ", numUniqueClick=" + numUniqueClick + ", numGrossClick="
+				+ numGrossClick + ", numUniqueAttach=" + numUniqueAttach + ", numGrossAttach=" + numGrossAttach
+				+ ", numUniqueClickstreams=" + numUniqueClickstreams + ", numGrossClickstreams=" + numGrossClickstreams
+				+ ", numUniqueMedia=" + numUniqueMedia + ", numGrossMedia=" + numGrossMedia + ", numGrossAbuse="
+				+ numGrossAbuse + ", numGrossChangeAddress=" + numGrossChangeAddress + ", numGrossMailBlock="
+				+ numGrossMailBlock + ", numGrossMailRestriction=" + numGrossMailRestriction + ", numGrossOther="
+				+ numGrossOther + ", numConversions=" + numConversions + ", numConversionAmount=" + numConversionAmount
+				+ ", numBounceHardFwd=" + numBounceHardFwd + ", numBounceSoftFwd=" + numBounceSoftFwd
+				+ ", numConversionAmountFwd=" + numConversionAmountFwd + ", numAttachOpenFwd=" + numAttachOpenFwd
+				+ ", numClickFwd=" + numClickFwd + ", numUniqueForwardFwd=" + numUniqueForwardFwd
+				+ ", numGrossForwardFwd=" + numGrossForwardFwd + ", numUniqueConversionsFwd=" + numUniqueConversionsFwd
+				+ ", numGrossConversionsFwd=" + numGrossConversionsFwd + ", numUniqueClickstreamFwd="
+				+ numUniqueClickstreamFwd + ", numGrossClickstreamFwd=" + numGrossClickstreamFwd
+				+ ", numUniqueClickFwd=" + numUniqueClickFwd + ", numGrossClickFwd=" + numGrossClickFwd
+				+ ", numUniqueAttachOpenFwd=" + numUniqueAttachOpenFwd + ", numGrossAttachOpenFwd="
+				+ numGrossAttachOpenFwd + ", numUniqueMediaFwd=" + numUniqueMediaFwd + ", numGrossMediaFwd="
+				+ numGrossMediaFwd + ", numUniqueOpenFwd=" + numUniqueOpenFwd + ", numGrossOpenFwd=" + numGrossOpenFwd
+				+ ", numAbuseFwd=" + numAbuseFwd + ", numChangeAddressFwd=" + numChangeAddressFwd
+				+ ", numMailRestrictionFwd=" + numMailRestrictionFwd + ", numMailBlockFwd=" + numMailBlockFwd
+				+ ", numOtherFwd=" + numOtherFwd + ", numSuppressedFwd=" + numSuppressedFwd + ", numUnsubscribes="
+				+ numUnsubscribes + "]";
+	}
+
+}
