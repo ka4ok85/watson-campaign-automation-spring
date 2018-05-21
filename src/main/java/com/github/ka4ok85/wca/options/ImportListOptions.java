b7eb39e6cd4318a4b0fdb45d0899e21f6866f920
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-05-21 17:42:12 -0400

    added ImportListOptions class

---
+++ src/main/java/com/github/ka4ok85/wca/options/ImportListOptions.java
@@ -0,0 +1,58 @@
+package com.github.ka4ok85.wca.options;
+
+import java.io.File;
+
+import com.github.ka4ok85.wca.constants.FileEncoding;
+
+public class ImportListOptions extends AbstractOptions {
+	private String mapFile;
+	private String sourceFile;
+	private FileEncoding fileEncoding = FileEncoding.UTF_8;
+
+	public ImportListOptions(String mapFile, String sourceFile) {
+		super();
+		File file = new File(mapFile);
+		if (mapFile == null || mapFile == "" || file.exists() == false || file.isDirectory()) {
+			throw new RuntimeException("Map File does not exist. Provided path to Map File = " + mapFile);
+		}
+
+		file = new File(sourceFile);
+		if (sourceFile == null || sourceFile == "" || file.exists() == false || file.isDirectory()) {
+			throw new RuntimeException("Source File does not exist. Provided path to Source File = " + sourceFile);
+		}
+
+		this.mapFile = mapFile;
+		this.sourceFile = sourceFile;
+	}
+
+	public String getMapFile() {
+		return mapFile;
+	}
+
+	public void setMapFile(String mapFile) {
+		this.mapFile = mapFile;
+	}
+
+	public String getSourceFile() {
+		return sourceFile;
+	}
+
+	public void setSourceFile(String sourceFile) {
+		this.sourceFile = sourceFile;
+	}
+
+	public FileEncoding getFileEncoding() {
+		return fileEncoding;
+	}
+
+	public void setFileEncoding(FileEncoding fileEncoding) {
+		this.fileEncoding = fileEncoding;
+	}
+
+	@Override
+	public String toString() {
+		return "ImportListOptions [mapFile=" + mapFile + ", sourceFile=" + sourceFile + ", fileEncoding=" + fileEncoding
+				+ "]";
+	}
+
+}
