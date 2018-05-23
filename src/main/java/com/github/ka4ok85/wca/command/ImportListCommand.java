82b607dc5e244b2ef26a6bbd8c0f353ca45b2645
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-05-23 17:27:59 -0400

    added logic for reading additional params from ImportList API response

--- src/main/java/com/github/ka4ok85/wca/command/ImportListCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/ImportListCommand.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Service;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
-import com.github.ka4ok85.wca.constants.FileEncoding;
 import com.github.ka4ok85.wca.exceptions.BadApiResultException;
 import com.github.ka4ok85.wca.exceptions.EngageApiException;
 import com.github.ka4ok85.wca.exceptions.FailedGetAccessTokenException;
@@ -36,7 +35,7 @@ public class ImportListCommand extends AbstractCommand<ImportListResponse, Impor
 
 	@Autowired
 	private ImportListResponse importListResponse;
-	
+
 	@Override
 	public ResponseContainer<ImportListResponse> executeCommand(ImportListOptions options)
 			throws FailedGetAccessTokenException, FaultApiResultException, BadApiResultException {
@@ -60,7 +59,7 @@ public class ImportListCommand extends AbstractCommand<ImportListResponse, Impor
 		Element fileEncoding = doc.createElement("FILE_ENCODING");
 		fileEncoding.setTextContent(options.getFileEncoding().value());
 		addChildNode(fileEncoding, currentNode);
-		
+
 		String xml = getXML();
 		log.debug("XML Request is {}", xml);
 		Node resultNode = runApi(xml);
@@ -69,9 +68,25 @@ public class ImportListCommand extends AbstractCommand<ImportListResponse, Impor
 		XPath xpath = factory.newXPath();
 
 		String description;
+		Long badAddressesNumber;
+		Long badRecordsNumber;
+		Long calculateListJobId;
+		Long duplicatesNumber;
+		String errorFileName;
+		Long listId;
+		String listName;
+		String listProgramLists;
+		Long notAllowedNumber;
+		String parentFolderName;
+		String resultsFileName;
+		boolean smsConsent;
+		Long sqlAddedNumber;
+		Long sqlUpdatedNumber;
+		String syncFieldsFromMappingFile;
+		Long totalRowsNumber;
+		Long totalValidNumber;
 		try {
 			Node jobIdNode = (Node) xpath.evaluate("JOB_ID", resultNode, XPathConstants.NODE);
-			Node filePathNode = (Node) xpath.evaluate("FILE_PATH", resultNode, XPathConstants.NODE);
 
 			final Long jobId = Long.parseLong(jobIdNode.getTextContent());
 			log.debug("Job ID {} is being excuted", jobId);
@@ -79,18 +94,24 @@ public class ImportListCommand extends AbstractCommand<ImportListResponse, Impor
 			final JobResponse jobResponse = waitUntilJobIsCompleted(jobId);
 			log.debug("Job Response is {}", jobResponse);
 			if (jobResponse.isComplete()) {
-				filePath = filePathNode.getTextContent();
 				description = jobResponse.getJobDescription();
+				badAddressesNumber = Long.valueOf(jobResponse.getParameters().get("BAD_ADDRESSES"));
+				badRecordsNumber = Long.valueOf(jobResponse.getParameters().get("BAD_RECORDS"));
+				calculateListJobId = Long.valueOf(jobResponse.getParameters().get("CALC_LIST_JOB_ID"));
+				duplicatesNumber = Long.valueOf(jobResponse.getParameters().get("DUPLICATES"));
+				errorFileName = jobResponse.getParameters().get("ERROR_FILE_NAME");
+				listId = Long.valueOf(jobResponse.getParameters().get("LIST_ID"));
 				listName = jobResponse.getParameters().get("LIST_NAME");
-				keepInStoredFiles = Boolean.valueOf(jobResponse.getParameters().get("KEEP_IN_STORED_FILES"));
-				keepInFtpDownloadDirectory = Boolean
-						.valueOf(jobResponse.getParameters().get("KEEP_IN_FTP_DOWNLOAD_DIRECTORY"));
-				fileEncodingValue = FileEncoding.getFileEncoding(jobResponse.getParameters().get("FILE_ENCODING"));
-
-				log.debug("Generated Export File {} on SFTP", filePath);
-				//if (options.getLocalAbsoluteFilePath() != null) {
-					//sftp.download(filePath, options.getLocalAbsoluteFilePath());
-				//}
+				listProgramLists = jobResponse.getParameters().get("LIST_PROGRAM_LISTS");
+				notAllowedNumber = Long.valueOf(jobResponse.getParameters().get("NOT_ALLOWED"));
+				parentFolderName = jobResponse.getParameters().get("PARENT_FOLDER_NAME");
+				resultsFileName = jobResponse.getParameters().get("RESULTS_FILE_NAME");
+				smsConsent = Boolean.valueOf(jobResponse.getParameters().get("CONSENT"));
+				sqlAddedNumber = Long.valueOf(jobResponse.getParameters().get("SQL_ADDED"));
+				sqlUpdatedNumber = Long.valueOf(jobResponse.getParameters().get("SQL_UPDATED"));
+				syncFieldsFromMappingFile = jobResponse.getParameters().get("SYNC_FIELDS_FROM_MAPPING_FILE");
+				totalRowsNumber = Long.valueOf(jobResponse.getParameters().get("TOTAL_ROWS"));
+				totalValidNumber = Long.valueOf(jobResponse.getParameters().get("TOTAL_VALID"));
 			} else {
 				log.error("State inconsistency for Job ID {}", jobId);
 				throw new JobBadStateException("Job ID " + jobId + " was reported as Completed, but actual State is "
@@ -99,44 +120,26 @@ public class ImportListCommand extends AbstractCommand<ImportListResponse, Impor
 		} catch (XPathExpressionException | JobBadStateException e) {
 			throw new EngageApiException(e.getMessage());
 		}
-		
-		Long badAddressesNumber;
-		Long badRecordsNumber;
-		importListResponse.setBadAddressesNumber(badAddressesNumber);
 
+		importListResponse.setBadAddressesNumber(badAddressesNumber);
 		importListResponse.setBadRecordsNumber(badRecordsNumber);
-		Long calculateListJobId;
 		importListResponse.setCalculateListJobId(calculateListJobId);
 		importListResponse.setDescription(description);
-		Long duplicatesNumber;
 		importListResponse.setDuplicatesNumber(duplicatesNumber);
-		String errorFileName;
 		importListResponse.setErrorFileName(errorFileName);
-		Long listId;
 		importListResponse.setListId(listId);
-		String listName;
 		importListResponse.setListName(listName);
-		String listProgramLists;
 		importListResponse.setListProgramLists(listProgramLists);
-		Long notAllowedNumber;
 		importListResponse.setNotAllowedNumber(notAllowedNumber);
-		String parentFolderName;
 		importListResponse.setParentFolderName(parentFolderName);
-		String resultsFileName;
 		importListResponse.setResultsFileName(resultsFileName);
-		boolean smsConsent;
 		importListResponse.setSmsConsent(smsConsent);
-		Long sqlAddedNumber;
 		importListResponse.setSqlAddedNumber(sqlAddedNumber);
-		Long sqlUpdatedNumber;
 		importListResponse.setSqlUpdatedNumber(sqlUpdatedNumber);
-		String syncFieldsFromMappingFile;
 		importListResponse.setSyncFieldsFromMappingFile(syncFieldsFromMappingFile);
-		Long totalRowsNumber;
 		importListResponse.setTotalRowsNumber(totalRowsNumber);
-		Long totalValidNumber;
 		importListResponse.setTotalValidNumber(totalValidNumber);
-		
+
 		ResponseContainer<ImportListResponse> response = new ResponseContainer<ImportListResponse>(importListResponse);
 
 		return response;
