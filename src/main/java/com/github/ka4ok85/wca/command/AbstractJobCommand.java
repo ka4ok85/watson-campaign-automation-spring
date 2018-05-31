f1579fbef3b0d33c25cf4941926783a9eec47633
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-05-31 16:45:54 -0400

    added Abstract command for APIs with polling

---
+++ src/main/java/com/github/ka4ok85/wca/command/AbstractJobCommand.java
@@ -0,0 +1,76 @@
+package com.github.ka4ok85.wca.command;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import com.github.ka4ok85.wca.exceptions.EngageApiException;
+import com.github.ka4ok85.wca.exceptions.JobBadStateException;
+import com.github.ka4ok85.wca.options.AbstractOptions;
+import com.github.ka4ok85.wca.processor.JobProcessor;
+import com.github.ka4ok85.wca.response.AbstractResponse;
+import com.github.ka4ok85.wca.response.JobResponse;
+import com.github.ka4ok85.wca.response.ResponseContainer;
+import com.github.ka4ok85.wca.response.containers.JobPollingContainer;
+
+abstract public class AbstractJobCommand<T extends AbstractResponse, V extends AbstractOptions>
+		extends AbstractCommand<T, V> {
+
+	public abstract ResponseContainer<T> readResponse(JobPollingContainer jobPollingContainer, JobResponse jobResponse,
+			V options);
+
+	public ResponseContainer<T> executeCommand(V options) {
+		buildXmlRequest(options);
+		String xml = getXML();
+		log.debug("XML Request is {}", xml);
+
+		Node resultNode = runApi(xml);
+
+		JobPollingContainer jobPollingContainer = readStartPollingResponse(resultNode);
+
+		JobResponse jobResponse = JobProcessor.waitUntilJobIsCompleted(jobPollingContainer.getJobId(), oAuthClient,
+				sftp);
+
+		return readResponse(jobPollingContainer, jobResponse, options);
+	}
+
+	private JobPollingContainer readStartPollingResponse(Node resultNode) {
+		XPathFactory factory = XPathFactory.newInstance();
+		XPath xpath = factory.newXPath();
+		Long jobId;
+		Map<String, String> parameters = new HashMap<String, String>();
+		try {
+			Node jobIdNode = (Node) xpath.evaluate("JOB_ID", resultNode, XPathConstants.NODE);
+
+			NodeList parametersNode = (NodeList) xpath.evaluate("*", resultNode, XPathConstants.NODESET);
+			Node parameterNode;
+			String nodeName;
+			String nodeValue;
+			for (int i = 0; i < parametersNode.getLength(); i++) {
+				parameterNode = parametersNode.item(i);
+				nodeName = parameterNode.getNodeName();
+				nodeValue = parameterNode.getTextContent();
+				if (!nodeName.equals("SUCCESS") && !nodeName.equals("JOB_ID")) {
+					parameters.put(nodeName, nodeValue);
+				}
+			}
+
+			jobId = Long.parseLong(jobIdNode.getTextContent());
+		} catch (XPathExpressionException | JobBadStateException e) {
+			throw new EngageApiException(e.getMessage());
+		}
+
+		JobPollingContainer jobPollingContainer = new JobPollingContainer();
+		jobPollingContainer.setJobId(jobId);
+		jobPollingContainer.setParameters(parameters);
+
+		return jobPollingContainer;
+	}
+}
