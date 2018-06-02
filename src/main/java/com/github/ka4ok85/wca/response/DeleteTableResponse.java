a971bbc70ba43e32db9bc438d5b451bc1f2be361
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-06-02 10:01:21 -0400

    added more fields to DeleteTableResponse

--- src/main/java/com/github/ka4ok85/wca/response/DeleteTableResponse.java
+++ src/main/java/com/github/ka4ok85/wca/response/DeleteTableResponse.java
@@ -6,18 +6,75 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("prototype")
 public class DeleteTableResponse extends AbstractResponse {
-	private Long JobId;
+	private Long jobId;
+	private String description;
+	private Long numberRemoved;
+	private boolean ignoreDependentMailings;
+	private boolean isKeepListDetails;
+	private Long listId;
+	private String listName;
 
 	public Long getJobId() {
-		return JobId;
+		return jobId;
 	}
 
 	public void setJobId(Long jobId) {
-		JobId = jobId;
+		this.jobId = jobId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Long getNumberRemoved() {
+		return numberRemoved;
+	}
+
+	public void setNumberRemoved(Long numberRemoved) {
+		this.numberRemoved = numberRemoved;
+	}
+
+	public boolean isIgnoreDependentMailings() {
+		return ignoreDependentMailings;
+	}
+
+	public void setIgnoreDependentMailings(boolean ignoreDependentMailings) {
+		this.ignoreDependentMailings = ignoreDependentMailings;
+	}
+
+	public boolean isKeepListDetails() {
+		return isKeepListDetails;
+	}
+
+	public void setKeepListDetails(boolean isKeepListDetails) {
+		this.isKeepListDetails = isKeepListDetails;
+	}
+
+	public Long getListId() {
+		return listId;
+	}
+
+	public void setListId(Long listId) {
+		this.listId = listId;
+	}
+
+	public String getListName() {
+		return listName;
+	}
+
+	public void setListName(String listName) {
+		this.listName = listName;
 	}
 
 	@Override
 	public String toString() {
-		return "DeleteTableResponse [JobId=" + JobId + "]";
+		return "DeleteTableResponse [jobId=" + jobId + ", description=" + description + ", numberRemoved="
+				+ numberRemoved + ", ignoreDependentMailings=" + ignoreDependentMailings + ", isKeepListDetails="
+				+ isKeepListDetails + ", listId=" + listId + ", listName=" + listName + "]";
 	}
+
 }
