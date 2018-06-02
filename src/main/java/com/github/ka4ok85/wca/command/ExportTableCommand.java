5b341d2c1d5688cab89acc4e312c2ad93a0ce615
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-06-02 15:53:10 -0400

    refactored ExportTableCommand to extend specific abstract job command

--- src/main/java/com/github/ka4ok85/wca/command/ExportTableCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/ExportTableCommand.java
@@ -2,30 +2,23 @@ package com.github.ka4ok85.wca.command;
 
 import java.util.Objects;
 
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 
-import com.github.ka4ok85.wca.exceptions.EngageApiException;
-import com.github.ka4ok85.wca.exceptions.JobBadStateException;
 import com.github.ka4ok85.wca.options.ExportTableOptions;
 import com.github.ka4ok85.wca.response.ExportTableResponse;
 import com.github.ka4ok85.wca.response.JobResponse;
 import com.github.ka4ok85.wca.response.ResponseContainer;
+import com.github.ka4ok85.wca.response.containers.JobPollingContainer;
 import com.github.ka4ok85.wca.utils.DateTimeRange;
 
 @Service
 @Scope("prototype")
-public class ExportTableCommand extends AbstractCommand<ExportTableResponse, ExportTableOptions> {
+public class ExportTableCommand extends AbstractJobCommand<ExportTableResponse, ExportTableOptions> {
 
 	private static final String apiMethodName = "ExportTable";
 	private static final Logger log = LoggerFactory.getLogger(ExportTableCommand.class);
@@ -62,44 +55,18 @@ public class ExportTableCommand extends AbstractCommand<ExportTableResponse, Exp
 	}
 
 	@Override
-	public ResponseContainer<ExportTableResponse> readResponse(Node resultNode, ExportTableOptions options) {
-		XPathFactory factory = XPathFactory.newInstance();
-		XPath xpath = factory.newXPath();
+	public ResponseContainer<ExportTableResponse> readResponse(JobPollingContainer jobPollingContainer,
+			JobResponse jobResponse, ExportTableOptions options) {
+		String description = jobResponse.getJobDescription();
+		String listName = jobResponse.getParameters().get("LIST_NAME");
+		String remoteFileName = jobResponse.getParameters().get("FILENAME");
+		Long listId = Long.parseLong(jobResponse.getParameters().get("LIST_ID"));
+		Long numProcessed = Long.parseLong(jobResponse.getParameters().get("NUM_PROCESSED"));
+		String filePath = jobPollingContainer.getParameters().get("FILE_PATH");
 
-		String description;
-		String remoteFileName;
-		String listName;
-		Long listId;
-		Long numProcessed;
-		String filePath;
-		try {
-			Node jobIdNode = (Node) xpath.evaluate("JOB_ID", resultNode, XPathConstants.NODE);
-			Node filePathNode = (Node) xpath.evaluate("FILE_PATH", resultNode, XPathConstants.NODE);
-
-			final Long jobId = Long.parseLong(jobIdNode.getTextContent());
-			log.debug("Job ID {} is being excuted", jobId);
-
-			final JobResponse jobResponse = waitUntilJobIsCompleted(jobId);
-			log.debug("Job Response is {}", jobResponse);
-			if (jobResponse.isComplete()) {
-				description = jobResponse.getJobDescription();
-				listName = jobResponse.getParameters().get("LIST_NAME");
-				remoteFileName = jobResponse.getParameters().get("FILENAME");
-				listId = Long.parseLong(jobResponse.getParameters().get("LIST_ID"));
-				numProcessed = Long.parseLong(jobResponse.getParameters().get("NUM_PROCESSED"));
-
-				filePath = filePathNode.getTextContent();
-				log.debug("Generated Export File {} on SFTP", filePath);
-				if (options.getLocalAbsoluteFilePath() != null) {
-					sftp.download(filePath, options.getLocalAbsoluteFilePath());
-				}
-			} else {
-				log.error("State inconsistency for Job ID {}", jobId);
-				throw new JobBadStateException("Job ID " + jobId + " was reported as Completed, but actual State is "
-						+ jobResponse.getJobStatus());
-			}
-		} catch (XPathExpressionException | JobBadStateException e) {
-			throw new EngageApiException(e.getMessage());
+		log.debug("Generated Export File {} on SFTP", filePath);
+		if (options.getLocalAbsoluteFilePath() != null) {
+			sftp.download(filePath, options.getLocalAbsoluteFilePath());
 		}
 
 		exportTableResponse.setDescription(description);
@@ -113,4 +80,5 @@ public class ExportTableCommand extends AbstractCommand<ExportTableResponse, Exp
 
 		return response;
 	}
+
 }
