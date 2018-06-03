22410fa2e9e9b5901ab2a47a2d771193d3daf5e3
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-06-03 14:20:25 -0400

    updated ImportList to extend AbstractJobCommand

--- src/main/java/com/github/ka4ok85/wca/command/ImportListCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/ImportListCommand.java
@@ -3,32 +3,22 @@ package com.github.ka4ok85.wca.command;
 import java.io.File;
 import java.util.Objects;
 
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 
-import com.github.ka4ok85.wca.exceptions.EngageApiException;
-import com.github.ka4ok85.wca.exceptions.JobBadStateException;
 import com.github.ka4ok85.wca.options.ImportListOptions;
 import com.github.ka4ok85.wca.response.ImportListResponse;
 import com.github.ka4ok85.wca.response.JobResponse;
 import com.github.ka4ok85.wca.response.ResponseContainer;
+import com.github.ka4ok85.wca.response.containers.JobPollingContainer;
 
 @Service
 @Scope("prototype")
-public class ImportListCommand extends AbstractCommand<ImportListResponse, ImportListOptions> {
+public class ImportListCommand extends AbstractJobCommand<ImportListResponse, ImportListOptions> {
 
 	private static final String apiMethodName = "ImportList";
-	private static final Logger log = LoggerFactory.getLogger(ImportListCommand.class);
 
 	@Autowired
 	private ImportListResponse importListResponse;
@@ -55,67 +45,29 @@ public class ImportListCommand extends AbstractCommand<ImportListResponse, Impor
 		Element fileEncoding = doc.createElement("FILE_ENCODING");
 		fileEncoding.setTextContent(options.getFileEncoding().value());
 		addChildNode(fileEncoding, currentNode);
-
 	}
 
 	@Override
-	public ResponseContainer<ImportListResponse> readResponse(Node resultNode, ImportListOptions options) {
-		XPathFactory factory = XPathFactory.newInstance();
-		XPath xpath = factory.newXPath();
-
-		String description;
-		Long badAddressesNumber;
-		Long badRecordsNumber;
-		Long calculateListJobId;
-		Long duplicatesNumber;
-		String errorFileName;
-		Long listId;
-		String listName;
-		String listProgramLists;
-		Long notAllowedNumber;
-		String parentFolderName;
-		String resultsFileName;
-		boolean smsConsent;
-		Long sqlAddedNumber;
-		Long sqlUpdatedNumber;
-		String syncFieldsFromMappingFile;
-		Long totalRowsNumber;
-		Long totalValidNumber;
-		try {
-			Node jobIdNode = (Node) xpath.evaluate("JOB_ID", resultNode, XPathConstants.NODE);
-
-			final Long jobId = Long.parseLong(jobIdNode.getTextContent());
-			log.debug("Job ID {} is being excuted", jobId);
-
-			final JobResponse jobResponse = waitUntilJobIsCompleted(jobId);
-			log.debug("Job Response is {}", jobResponse);
-			if (jobResponse.isComplete()) {
-				description = jobResponse.getJobDescription();
-				badAddressesNumber = Long.valueOf(jobResponse.getParameters().get("BAD_ADDRESSES"));
-				badRecordsNumber = Long.valueOf(jobResponse.getParameters().get("BAD_RECORDS"));
-				calculateListJobId = Long.valueOf(jobResponse.getParameters().get("CALC_LIST_JOB_ID"));
-				duplicatesNumber = Long.valueOf(jobResponse.getParameters().get("DUPLICATES"));
-				errorFileName = jobResponse.getParameters().get("ERROR_FILE_NAME");
-				listId = Long.valueOf(jobResponse.getParameters().get("LIST_ID"));
-				listName = jobResponse.getParameters().get("LIST_NAME");
-				listProgramLists = jobResponse.getParameters().get("LIST_PROGRAM_LISTS");
-				notAllowedNumber = Long.valueOf(jobResponse.getParameters().get("NOT_ALLOWED"));
-				parentFolderName = jobResponse.getParameters().get("PARENT_FOLDER_NAME");
-				resultsFileName = jobResponse.getParameters().get("RESULTS_FILE_NAME");
-				smsConsent = Boolean.valueOf(jobResponse.getParameters().get("CONSENT"));
-				sqlAddedNumber = Long.valueOf(jobResponse.getParameters().get("SQL_ADDED"));
-				sqlUpdatedNumber = Long.valueOf(jobResponse.getParameters().get("SQL_UPDATED"));
-				syncFieldsFromMappingFile = jobResponse.getParameters().get("SYNC_FIELDS_FROM_MAPPING_FILE");
-				totalRowsNumber = Long.valueOf(jobResponse.getParameters().get("TOTAL_ROWS"));
-				totalValidNumber = Long.valueOf(jobResponse.getParameters().get("TOTAL_VALID"));
-			} else {
-				log.error("State inconsistency for Job ID {}", jobId);
-				throw new JobBadStateException("Job ID " + jobId + " was reported as Completed, but actual State is "
-						+ jobResponse.getJobStatus());
-			}
-		} catch (XPathExpressionException | JobBadStateException e) {
-			throw new EngageApiException(e.getMessage());
-		}
+	public ResponseContainer<ImportListResponse> readResponse(JobPollingContainer jobPollingContainer,
+			JobResponse jobResponse, ImportListOptions options) {
+		String description = jobResponse.getJobDescription();
+		Long badAddressesNumber = Long.valueOf(jobResponse.getParameters().get("BAD_ADDRESSES"));
+		Long badRecordsNumber = Long.valueOf(jobResponse.getParameters().get("BAD_RECORDS"));
+		Long calculateListJobId = Long.valueOf(jobResponse.getParameters().get("CALC_LIST_JOB_ID"));
+		Long duplicatesNumber = Long.valueOf(jobResponse.getParameters().get("DUPLICATES"));
+		String errorFileName = jobResponse.getParameters().get("ERROR_FILE_NAME");
+		Long listId = Long.valueOf(jobResponse.getParameters().get("LIST_ID"));
+		String listName = jobResponse.getParameters().get("LIST_NAME");
+		String listProgramLists = jobResponse.getParameters().get("LIST_PROGRAM_LISTS");
+		Long notAllowedNumber = Long.valueOf(jobResponse.getParameters().get("NOT_ALLOWED"));
+		String parentFolderName = jobResponse.getParameters().get("PARENT_FOLDER_NAME");
+		String resultsFileName = jobResponse.getParameters().get("RESULTS_FILE_NAME");
+		boolean smsConsent = Boolean.valueOf(jobResponse.getParameters().get("CONSENT"));
+		Long sqlAddedNumber = Long.valueOf(jobResponse.getParameters().get("SQL_ADDED"));
+		Long sqlUpdatedNumber = Long.valueOf(jobResponse.getParameters().get("SQL_UPDATED"));
+		String syncFieldsFromMappingFile = jobResponse.getParameters().get("SYNC_FIELDS_FROM_MAPPING_FILE");
+		Long totalRowsNumber = Long.valueOf(jobResponse.getParameters().get("TOTAL_ROWS"));
+		Long totalValidNumber = Long.valueOf(jobResponse.getParameters().get("TOTAL_VALID"));
 
 		importListResponse.setBadAddressesNumber(badAddressesNumber);
 		importListResponse.setBadRecordsNumber(badRecordsNumber);
