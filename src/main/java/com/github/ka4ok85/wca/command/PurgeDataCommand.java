e2e717ef1f24e6ec4568db8bfd4cded81ca9d243
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-06-04 17:45:18 -0400

    updated PurgeData class to extend AbstractJobCommand

--- src/main/java/com/github/ka4ok85/wca/command/PurgeDataCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/PurgeDataCommand.java
@@ -2,32 +2,22 @@ package com.github.ka4ok85.wca.command;
 
 import java.util.Objects;
 
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 
-import com.github.ka4ok85.wca.exceptions.EngageApiException;
-import com.github.ka4ok85.wca.exceptions.JobBadStateException;
 import com.github.ka4ok85.wca.options.PurgeDataOptions;
 import com.github.ka4ok85.wca.response.JobResponse;
 import com.github.ka4ok85.wca.response.PurgeDataResponse;
 import com.github.ka4ok85.wca.response.ResponseContainer;
+import com.github.ka4ok85.wca.response.containers.JobPollingContainer;
 
 @Service
 @Scope("prototype")
-public class PurgeDataCommand extends AbstractCommand<PurgeDataResponse, PurgeDataOptions> {
+public class PurgeDataCommand extends AbstractJobCommand<PurgeDataResponse, PurgeDataOptions> {
 
 	private static final String apiMethodName = "PurgeData";
-	private static final Logger log = LoggerFactory.getLogger(PurgeDataCommand.class);
 
 	@Autowired
 	private PurgeDataResponse purgeDataResponse;
@@ -49,39 +39,14 @@ public class PurgeDataCommand extends AbstractCommand<PurgeDataResponse, PurgeDa
 	}
 
 	@Override
-	public ResponseContainer<PurgeDataResponse> readResponse(Node resultNode, PurgeDataOptions options) {
-		XPathFactory factory = XPathFactory.newInstance();
-		XPath xpath = factory.newXPath();
-		String description;
-		String purgedContactListName;
-		Long purgedContactListId;
-		Long purgedContactListSize;
-		String targetListName;
-		String sourceListName;
-
-		try {
-			Node jobIdNode = (Node) xpath.evaluate("JOB_ID", resultNode, XPathConstants.NODE);
-
-			final Long jobId = Long.parseLong(jobIdNode.getTextContent());
-			log.debug("Job ID {} is being excuted", jobId);
-
-			final JobResponse jobResponse = waitUntilJobIsCompleted(jobId);
-			log.debug("Job Response is {}", jobResponse);
-			if (jobResponse.isComplete()) {
-				description = jobResponse.getJobDescription();
-				purgedContactListId = Long.parseLong(jobResponse.getParameters().get("LIST_ID_C"));
-				purgedContactListName = jobResponse.getParameters().get("LIST_NAME_C");
-				purgedContactListSize = Long.parseLong(jobResponse.getParameters().get("LIST_SIZE"));
-				sourceListName = jobResponse.getParameters().get("LIST2_NAME");
-				targetListName = jobResponse.getParameters().get("LIST1_NAME");
-			} else {
-				log.error("State inconsistency for Job ID {}", jobId);
-				throw new JobBadStateException("Job ID " + jobId + " was reported as Completed, but actual State is "
-						+ jobResponse.getJobStatus());
-			}
-		} catch (XPathExpressionException | JobBadStateException e) {
-			throw new EngageApiException(e.getMessage());
-		}
+	public ResponseContainer<PurgeDataResponse> readResponse(JobPollingContainer jobPollingContainer,
+			JobResponse jobResponse, PurgeDataOptions options) {
+		String description = jobResponse.getJobDescription();
+		Long purgedContactListId = Long.parseLong(jobResponse.getParameters().get("LIST_ID_C"));
+		String purgedContactListName = jobResponse.getParameters().get("LIST_NAME_C");
+		Long purgedContactListSize = Long.parseLong(jobResponse.getParameters().get("LIST_SIZE"));
+		String sourceListName = jobResponse.getParameters().get("LIST2_NAME");
+		String targetListName = jobResponse.getParameters().get("LIST1_NAME");
 
 		purgeDataResponse.setDescription(description);
 		purgeDataResponse.setPurgedContactListId(purgedContactListId);
@@ -94,4 +59,5 @@ public class PurgeDataCommand extends AbstractCommand<PurgeDataResponse, PurgeDa
 
 		return response;
 	}
+
 }
