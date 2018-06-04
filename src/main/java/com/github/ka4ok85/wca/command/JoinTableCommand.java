611e83779f5206a0cc18972ec8641500c969b6cc
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-06-04 16:22:21 -0400

    refactored JoinTableCommand

--- src/main/java/com/github/ka4ok85/wca/command/JoinTableCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/JoinTableCommand.java
@@ -3,43 +3,29 @@ package com.github.ka4ok85.wca.command;
 import java.util.Map.Entry;
 import java.util.Objects;
 
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.CDATASection;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 
-import com.github.ka4ok85.wca.exceptions.BadApiResultException;
-import com.github.ka4ok85.wca.exceptions.EngageApiException;
-import com.github.ka4ok85.wca.exceptions.FailedGetAccessTokenException;
-import com.github.ka4ok85.wca.exceptions.FaultApiResultException;
-import com.github.ka4ok85.wca.exceptions.JobBadStateException;
 import com.github.ka4ok85.wca.options.JoinTableOptions;
 import com.github.ka4ok85.wca.response.JobResponse;
 import com.github.ka4ok85.wca.response.JoinTableResponse;
 import com.github.ka4ok85.wca.response.ResponseContainer;
+import com.github.ka4ok85.wca.response.containers.JobPollingContainer;
 
 @Service
 @Scope("prototype")
-public class JoinTableCommand extends AbstractCommand<JoinTableResponse, JoinTableOptions> {
+public class JoinTableCommand extends AbstractJobCommand<JoinTableResponse, JoinTableOptions> {
 
 	private static final String apiMethodName = "JoinTable";
-	private static final Logger log = LoggerFactory.getLogger(JoinTableCommand.class);
 
 	@Autowired
 	private JoinTableResponse joinTableResponse;
 
 	@Override
-	public ResponseContainer<JoinTableResponse> executeCommand(JoinTableOptions options)
-			throws FailedGetAccessTokenException, FaultApiResultException, BadApiResultException {
+	public void buildXmlRequest(JoinTableOptions options) {
 		Objects.requireNonNull(options, "JoinTableOptions must not be null");
 
 		Element methodElement = doc.createElement(apiMethodName);
@@ -99,39 +85,18 @@ public class JoinTableCommand extends AbstractCommand<JoinTableResponse, JoinTab
 			tableColumn.appendChild(cdata);
 			addChildNode(tableColumn, mapField);
 		}
+	}
 
-		String xml = getXML();
-		log.debug("XML Request is {}", xml);
-		Node resultNode = runApi(xml);
+	@Override
+	public ResponseContainer<JoinTableResponse> readResponse(JobPollingContainer jobPollingContainer,
+			JobResponse jobResponse, JoinTableOptions options) {
 
-		XPathFactory factory = XPathFactory.newInstance();
-		XPath xpath = factory.newXPath();
-		Long jobId;
-		String description;
-		try {
-			Node jobIdNode = (Node) xpath.evaluate("JOB_ID", resultNode, XPathConstants.NODE);
-
-			jobId = Long.parseLong(jobIdNode.getTextContent());
-			log.debug("Job ID {} is being excuted", jobId);
-
-			final JobResponse jobResponse = waitUntilJobIsCompleted(jobId);
-			log.debug("Job Response is {}", jobResponse);
-			if (jobResponse.isComplete()) {
-				description = jobResponse.getJobDescription();
-				log.debug("Job is completed");
-			} else {
-				log.error("State inconsistency for Job ID {}", jobId);
-				throw new JobBadStateException("Job ID " + jobId + " was reported as Completed, but actual State is "
-						+ jobResponse.getJobStatus());
-			}
-		} catch (XPathExpressionException | JobBadStateException e) {
-			throw new EngageApiException(e.getMessage());
-		}
-
-		joinTableResponse.setJobId(jobId);
+		String description = jobResponse.getJobDescription();
+		joinTableResponse.setJobId(jobPollingContainer.getJobId());
 		joinTableResponse.setDescription(description);
 		ResponseContainer<JoinTableResponse> response = new ResponseContainer<JoinTableResponse>(joinTableResponse);
 
 		return response;
 	}
+
 }
