6dd6449875cc180c6954f2380fff3a8172818cd6
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-06-06 10:25:28 -0400

    refactored executeCommand() method

--- src/main/java/com/github/ka4ok85/wca/command/PurgeTableCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/PurgeTableCommand.java
@@ -3,42 +3,28 @@ package com.github.ka4ok85.wca.command;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 
-import com.github.ka4ok85.wca.exceptions.BadApiResultException;
-import com.github.ka4ok85.wca.exceptions.EngageApiException;
-import com.github.ka4ok85.wca.exceptions.FailedGetAccessTokenException;
-import com.github.ka4ok85.wca.exceptions.FaultApiResultException;
-import com.github.ka4ok85.wca.exceptions.JobBadStateException;
 import com.github.ka4ok85.wca.options.PurgeTableOptions;
 import com.github.ka4ok85.wca.response.JobResponse;
 import com.github.ka4ok85.wca.response.PurgeTableResponse;
 import com.github.ka4ok85.wca.response.ResponseContainer;
+import com.github.ka4ok85.wca.response.containers.JobPollingContainer;
 
 @Service
 @Scope("prototype")
-public class PurgeTableCommand extends AbstractCommand<PurgeTableResponse, PurgeTableOptions> {
+public class PurgeTableCommand extends AbstractJobCommand<PurgeTableResponse, PurgeTableOptions> {
 
 	private static final String apiMethodName = "PurgeTable";
-	private static final Logger log = LoggerFactory.getLogger(PurgeTableCommand.class);
 
 	@Autowired
 	private PurgeTableResponse purgeTableResponse;
 
 	@Override
-	public ResponseContainer<PurgeTableResponse> executeCommand(PurgeTableOptions options)
-			throws FailedGetAccessTokenException, FaultApiResultException, BadApiResultException {
+	public void buildXmlRequest(PurgeTableOptions options) {
 		Objects.requireNonNull(options, "PurgeTableOptions must not be null");
 
 		Element methodElement = doc.createElement(apiMethodName);
@@ -68,34 +54,13 @@ public class PurgeTableCommand extends AbstractCommand<PurgeTableResponse, Purge
 			deleteBefore.setTextContent(options.getDeleteBefore().format(formatter));
 			addChildNode(deleteBefore, currentNode);
 		}
+	}
 
-		String xml = getXML();
-		log.debug("XML Request is {}", xml);
-		Node resultNode = runApi(xml);
-
-		XPathFactory factory = XPathFactory.newInstance();
-		XPath xpath = factory.newXPath();
-		Long jobId;
-		String description;
-		try {
-			Node jobIdNode = (Node) xpath.evaluate("JOB_ID", resultNode, XPathConstants.NODE);
-
-			jobId = Long.parseLong(jobIdNode.getTextContent());
-			log.debug("Job ID {} is being excuted", jobId);
-
-			final JobResponse jobResponse = waitUntilJobIsCompleted(jobId);
-			log.debug("Job Response is {}", jobResponse);
-			if (jobResponse.isComplete()) {
-				description = jobResponse.getJobDescription();
-				log.debug("Job is completed");
-			} else {
-				log.error("State inconsistency for Job ID {}", jobId);
-				throw new JobBadStateException("Job ID " + jobId + " was reported as Completed, but actual State is "
-						+ jobResponse.getJobStatus());
-			}
-		} catch (XPathExpressionException | JobBadStateException e) {
-			throw new EngageApiException(e.getMessage());
-		}
+	@Override
+	public ResponseContainer<PurgeTableResponse> readResponse(JobPollingContainer jobPollingContainer,
+			JobResponse jobResponse, PurgeTableOptions options) {
+		Long jobId = jobPollingContainer.getJobId();
+		String description = jobResponse.getJobDescription();
 
 		purgeTableResponse.setJobId(jobId);
 		purgeTableResponse.setDescription(description);
@@ -103,4 +68,5 @@ public class PurgeTableCommand extends AbstractCommand<PurgeTableResponse, Purge
 
 		return response;
 	}
+
 }
