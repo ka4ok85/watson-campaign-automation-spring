687b96b87562b4baff2004b4bb39b6eb1863f313
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-06-07 16:53:36 -0400

    added unit tests for PurgeTableCommand

--- src/test/java/com/github/ka4ok85/wca/command/PurgeTableCommandTest.java
+++ src/test/java/com/github/ka4ok85/wca/command/PurgeTableCommandTest.java
@@ -2,8 +2,8 @@ package com.github.ka4ok85.wca.command;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.xml.transform.Source;
 
@@ -20,40 +20,36 @@ import org.xmlunit.diff.Diff;
 
 import com.github.ka4ok85.wca.config.SpringConfig;
 import com.github.ka4ok85.wca.constants.Visibility;
-import com.github.ka4ok85.wca.options.JoinTableOptions;
+import com.github.ka4ok85.wca.options.PurgeTableOptions;
 import com.github.ka4ok85.wca.response.JobResponse;
-import com.github.ka4ok85.wca.response.JoinTableResponse;
+import com.github.ka4ok85.wca.response.PurgeTableResponse;
 import com.github.ka4ok85.wca.response.ResponseContainer;
 import com.github.ka4ok85.wca.response.containers.JobPollingContainer;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { SpringConfig.class })
-public class JoinTableCommandTest {
+public class PurgeTableCommandTest {
 
 	@Autowired
 	ApplicationContext context;
 
 	private String defaultRequest = String.join(System.getProperty("line.separator"),
-			"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>", "<Envelope>", "<Body>", "<JoinTable>",
-			"<TABLE_ID>1</TABLE_ID>", "<LIST_ID>2</LIST_ID>", "<MAP_FIELD>", "<LIST_FIELD><![CDATA[rid]]></LIST_FIELD>",
-			"<TABLE_FIELD><![CDATA[recipid]]></TABLE_FIELD>", "</MAP_FIELD>", "</JoinTable>", "</Body>", "</Envelope>");
+			"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>", "<Envelope>", "<Body>", "<PurgeTable>",
+			"<TABLE_ID>1</TABLE_ID>", "</PurgeTable>", "</Body>", "</Envelope>");
 
 	@Test(expected = NullPointerException.class)
 	public void testBuildXmlDoesNotAcceptNullOptions() {
-		JoinTableCommand command = new JoinTableCommand();
-		JoinTableOptions options = null;
+		PurgeTableCommand command = new PurgeTableCommand();
+		PurgeTableOptions options = null;
 		command.buildXmlRequest(options);
 	}
 
 	@Test
 	public void testBuildXmlDefaultRequest() {
 		// get XML from command
-		JoinTableCommand command = new JoinTableCommand();
+		PurgeTableCommand command = new PurgeTableCommand();
 
-		Map<String, String> mapFields = new HashMap<String, String>();
-		mapFields.put("rid", "recipid");
-		JoinTableOptions options = new JoinTableOptions(mapFields);
-		options.setListId(2L);
+		PurgeTableOptions options = new PurgeTableOptions();
 		options.setTableId(1L);
 		command.buildXmlRequest(options);
 		String testString = command.getXML();
@@ -70,14 +66,10 @@ public class JoinTableCommandTest {
 	@Test
 	public void testBuildXmlAcceptsNames() {
 		// get XML from command
-		JoinTableCommand command = new JoinTableCommand();
+		PurgeTableCommand command = new PurgeTableCommand();
 
-		Map<String, String> mapFields = new HashMap<String, String>();
-		mapFields.put("rid", "recipid");
-		JoinTableOptions options = new JoinTableOptions(mapFields);
-		String listName = "test list 1";
-		options.setListName(listName);
-		String tableName = "test table 2";
+		PurgeTableOptions options = new PurgeTableOptions();
+		String tableName = "test table 1";
 		options.setTableName(tableName);
 		command.buildXmlRequest(options);
 		String testString = command.getXML();
@@ -86,7 +78,6 @@ public class JoinTableCommandTest {
 		// get control XML
 		String controlString = defaultRequest.replace("<TABLE_ID>1</TABLE_ID>",
 				"<TABLE_NAME>" + tableName + "</TABLE_NAME>");
-		controlString = controlString.replace("<LIST_ID>2</LIST_ID>", "<LIST_NAME>" + listName + "</LIST_NAME>");
 		Source control = Input.fromString(controlString).build();
 
 		Diff myDiff = DiffBuilder.compare(control).withTest(test).ignoreWhitespace().checkForSimilar().build();
@@ -94,36 +85,19 @@ public class JoinTableCommandTest {
 	}
 
 	@Test(expected = RuntimeException.class)
-	public void testBuildXmlRequiresListIdOrListName() {
-		JoinTableCommand command = new JoinTableCommand();
-
-		Map<String, String> mapFields = new HashMap<String, String>();
-		mapFields.put("rid", "recipid");
-		JoinTableOptions options = new JoinTableOptions(mapFields);
-		String tableName = "test table 2";
-		options.setTableName(tableName);
-		command.buildXmlRequest(options);
-	}
-
-	@Test(expected = RuntimeException.class)
 	public void testBuildXmlRequiresTableIdOrTableName() {
-		JoinTableCommand command = new JoinTableCommand();
+		PurgeTableCommand command = new PurgeTableCommand();
 
-		Map<String, String> mapFields = new HashMap<String, String>();
-		mapFields.put("rid", "recipid");
-		JoinTableOptions options = new JoinTableOptions(mapFields);
+		PurgeTableOptions options = new PurgeTableOptions();
 		command.buildXmlRequest(options);
 	}
 
 	@Test
 	public void testBuildXmlHonorsTableVisibility() {
 		// get XML from command
-		JoinTableCommand command = new JoinTableCommand();
+		PurgeTableCommand command = new PurgeTableCommand();
 
-		Map<String, String> mapFields = new HashMap<String, String>();
-		mapFields.put("rid", "recipid");
-		JoinTableOptions options = new JoinTableOptions(mapFields);
-		options.setListId(2L);
+		PurgeTableOptions options = new PurgeTableOptions();
 		options.setTableId(1L);
 
 		Visibility tableVisibility = Visibility.SHARED;
@@ -142,49 +116,23 @@ public class JoinTableCommandTest {
 	}
 
 	@Test
-	public void testBuildXmlHonorsListVisibility() {
+	public void testBuildXmlHonorsDeleteBefore() {
 		// get XML from command
-		JoinTableCommand command = new JoinTableCommand();
+		PurgeTableCommand command = new PurgeTableCommand();
 
-		Map<String, String> mapFields = new HashMap<String, String>();
-		mapFields.put("rid", "recipid");
-		JoinTableOptions options = new JoinTableOptions(mapFields);
-		options.setListId(2L);
+		PurgeTableOptions options = new PurgeTableOptions();
 		options.setTableId(1L);
+		LocalDateTime deleteBefore = LocalDateTime.now();
+		options.setDeleteBefore(deleteBefore);
 
-		Visibility listVisibility = Visibility.PRIVATE;
-		options.setListVisibility(listVisibility);
 		command.buildXmlRequest(options);
 		String testString = command.getXML();
 		Source test = Input.fromString(testString).build();
 
 		// get control XML
-		String controlString = defaultRequest.replace("</LIST_ID>",
-				"</LIST_ID><LIST_VISIBILITY>" + listVisibility.value().toString() + "</LIST_VISIBILITY>");
-		Source control = Input.fromString(controlString).build();
-
-		Diff myDiff = DiffBuilder.compare(control).withTest(test).ignoreWhitespace().checkForSimilar().build();
-		Assert.assertFalse(myDiff.toString(), myDiff.hasDifferences());
-	}
-
-	@Test
-	public void testBuildXmlHonorsRemoveParam() {
-		// get XML from command
-		JoinTableCommand command = new JoinTableCommand();
-
-		Map<String, String> mapFields = new HashMap<String, String>();
-		mapFields.put("rid", "recipid");
-		JoinTableOptions options = new JoinTableOptions(mapFields);
-		options.setListId(2L);
-		options.setTableId(1L);
-
-		options.setIsRemoveRelationship(true);
-		command.buildXmlRequest(options);
-		String testString = command.getXML();
-		Source test = Input.fromString(testString).build();
-
-		// get control XML
-		String controlString = defaultRequest.replace("</LIST_ID>", "</LIST_ID><REMOVE>TRUE</REMOVE>");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");
+		String controlString = defaultRequest.replace("</TABLE_ID>",
+				"</TABLE_ID><DELETE_BEFORE>" + deleteBefore.format(formatter) + "</DELETE_BEFORE>");
 		Source control = Input.fromString(controlString).build();
 
 		Diff myDiff = DiffBuilder.compare(control).withTest(test).ignoreWhitespace().checkForSimilar().build();
@@ -193,12 +141,9 @@ public class JoinTableCommandTest {
 
 	@Test
 	public void testReadResponse() {
-		JoinTableCommand command = context.getBean(JoinTableCommand.class);
+		PurgeTableCommand command = context.getBean(PurgeTableCommand.class);
 
-		Map<String, String> mapFields = new HashMap<String, String>();
-		mapFields.put("rid", "recipid");
-		JoinTableOptions options = new JoinTableOptions(mapFields);
-		options.setListId(2L);
+		PurgeTableOptions options = new PurgeTableOptions();
 		options.setTableId(1L);
 
 		JobPollingContainer jobPollingContainer = new JobPollingContainer();
@@ -209,9 +154,9 @@ public class JoinTableCommandTest {
 		JobResponse jobResponse = new JobResponse();
 		jobResponse.setJobDescription(jobDescription);
 
-		ResponseContainer<JoinTableResponse> responseContainer = command.readResponse(jobPollingContainer, jobResponse,
+		ResponseContainer<PurgeTableResponse> responseContainer = command.readResponse(jobPollingContainer, jobResponse,
 				options);
-		JoinTableResponse response = responseContainer.getResposne();
+		PurgeTableResponse response = responseContainer.getResposne();
 
 		assertEquals(response.getJobId(), jobId);
 		assertEquals(response.getDescription(), jobDescription);
