32426e1dde624312d30a6027be181e31993d462a
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-06-09 14:10:25 -0400

    added Java doc for CalculateQueryCommand

--- src/main/java/com/github/ka4ok85/wca/command/CalculateQueryCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/CalculateQueryCommand.java
@@ -13,6 +13,23 @@ import com.github.ka4ok85.wca.response.JobResponse;
 import com.github.ka4ok85.wca.response.ResponseContainer;
 import com.github.ka4ok85.wca.response.containers.JobPollingContainer;
 
+/**
+ * <strong>Class for interacting with WCA CalculateQuery API.</strong> It builds XML
+ * request for CalculateQuery API using
+ * {@link com.github.ka4ok85.wca.options.CalculateQueryOptions} and reads response
+ * into {@link com.github.ka4ok85.wca.response.CalculateQueryResponse}.
+ * <p>
+ * It relies on Spring's {@link org.springframework.web.client.RestTemplate} for
+ * synchronous client-side HTTP access.
+ * </p>
+ * <p>
+ * It relies on parent {@link com.github.ka4ok85.wca.command.AbstractJobCommand}
+ * for polling mechanism.
+ * </p>
+ *
+ * @author Evgeny Makovetsky
+ * @since 0.0.2
+ */
 @Service
 @Scope("prototype")
 public class CalculateQueryCommand extends AbstractJobCommand<CalculateQueryResponse, CalculateQueryOptions> {
@@ -22,6 +39,14 @@ public class CalculateQueryCommand extends AbstractJobCommand<CalculateQueryResp
 	@Autowired
 	private CalculateQueryResponse calculateQueryResponse;
 
+	/**
+	 * Builds XML request for CalculateQuery API using
+	 * {@link com.github.ka4ok85.wca.options.CalculateQueryOptions}
+	 * 
+	 * @param options
+	 *            - settings for API call
+	 * @return void
+	 */
 	@Override
 	public void buildXmlRequest(CalculateQueryOptions options) {
 		Objects.requireNonNull(options, "CalculateQueryOptions must not be null");
@@ -34,6 +59,18 @@ public class CalculateQueryCommand extends AbstractJobCommand<CalculateQueryResp
 		addChildNode(queryId, currentNode);
 	}
 
+	/**
+	 * Reads CalculateQuery API response into
+	 * {@link com.github.ka4ok85.wca.response.CalculateQueryResponse}
+	 * 
+	 * @param jobPollingContainer
+	 *            - raw POJO response for Schedule a Job API
+	 * @param jobResponse
+	 *            - raw POJO response for CalculateQuery API
+	 * @param options
+	 *            - settings for API call
+	 * @return POJO Calculate Query Response
+	 */
 	@Override
 	public ResponseContainer<CalculateQueryResponse> readResponse(JobPollingContainer jobPollingContainer,
 			JobResponse jobResponse, CalculateQueryOptions options) {
