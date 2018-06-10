97ea075be1f8ea1f720c98a672e8970ac67e78cd
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-06-10 14:57:25 -0400

    more Javadocs added

--- src/main/java/com/github/ka4ok85/wca/command/DeleteTableCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/DeleteTableCommand.java
@@ -13,6 +13,23 @@ import com.github.ka4ok85.wca.response.JobResponse;
 import com.github.ka4ok85.wca.response.ResponseContainer;
 import com.github.ka4ok85.wca.response.containers.JobPollingContainer;
 
+/**
+ * <strong>Class for interacting with WCA DeleteTable API.</strong> It builds
+ * XML request for DeleteTable API using
+ * {@link com.github.ka4ok85.wca.options.DeleteTableOptions} and reads response
+ * into {@link com.github.ka4ok85.wca.response.DeleteTableResponse}.
+ * <p>
+ * It relies on Spring's {@link org.springframework.web.client.RestTemplate} for
+ * synchronous client-side HTTP access.
+ * </p>
+ * <p>
+ * It relies on parent {@link com.github.ka4ok85.wca.command.AbstractJobCommand}
+ * for polling mechanism.
+ * </p>
+ *
+ * @author Evgeny Makovetsky
+ * @since 0.0.2
+ */
 @Service
 @Scope("prototype")
 public class DeleteTableCommand extends AbstractJobCommand<DeleteTableResponse, DeleteTableOptions> {
@@ -22,6 +39,14 @@ public class DeleteTableCommand extends AbstractJobCommand<DeleteTableResponse, 
 	@Autowired
 	private DeleteTableResponse deleteTableResponse;
 
+	/**
+	 * Builds XML request for DeleteTable API using
+	 * {@link com.github.ka4ok85.wca.options.DeleteTableOptions}
+	 * 
+	 * @param options
+	 *            - settings for API call
+	 * @return void
+	 */
 	@Override
 	public void buildXmlRequest(DeleteTableOptions options) {
 		Objects.requireNonNull(options, "DeleteTableOptions must not be null");
@@ -48,6 +73,18 @@ public class DeleteTableCommand extends AbstractJobCommand<DeleteTableResponse, 
 		}
 	}
 
+	/**
+	 * Reads DeleteTable API response into
+	 * {@link com.github.ka4ok85.wca.response.DeleteTableResponse}
+	 * 
+	 * @param jobPollingContainer
+	 *            - raw POJO response for Schedule a Job API
+	 * @param jobResponse
+	 *            - raw POJO response for DeleteTable API
+	 * @param options
+	 *            - settings for API call
+	 * @return POJO Delete Table Response
+	 */
 	@Override
 	public ResponseContainer<DeleteTableResponse> readResponse(JobPollingContainer jobPollingContainer,
 			JobResponse jobResponse, DeleteTableOptions options) {
--- src/main/java/com/github/ka4ok85/wca/command/ImportListCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/ImportListCommand.java
@@ -14,6 +14,23 @@ import com.github.ka4ok85.wca.response.JobResponse;
 import com.github.ka4ok85.wca.response.ResponseContainer;
 import com.github.ka4ok85.wca.response.containers.JobPollingContainer;
 
+/**
+ * <strong>Class for interacting with WCA ImportList API.</strong> It builds XML
+ * request for ImportList API using
+ * {@link com.github.ka4ok85.wca.options.ImportListOptions} and reads response
+ * into {@link com.github.ka4ok85.wca.response.ImportListResponse}.
+ * <p>
+ * It relies on Spring's {@link org.springframework.web.client.RestTemplate} for
+ * synchronous client-side HTTP access.
+ * </p>
+ * <p>
+ * It relies on parent {@link com.github.ka4ok85.wca.command.AbstractJobCommand}
+ * for polling mechanism.
+ * </p>
+ *
+ * @author Evgeny Makovetsky
+ * @since 0.0.2
+ */
 @Service
 @Scope("prototype")
 public class ImportListCommand extends AbstractJobCommand<ImportListResponse, ImportListOptions> {
@@ -23,6 +40,14 @@ public class ImportListCommand extends AbstractJobCommand<ImportListResponse, Im
 	@Autowired
 	private ImportListResponse importListResponse;
 
+	/**
+	 * Builds XML request for ImportList API using
+	 * {@link com.github.ka4ok85.wca.options.ImportListOptions}
+	 * 
+	 * @param options
+	 *            - settings for API call
+	 * @return void
+	 */
 	@Override
 	public void buildXmlRequest(ImportListOptions options) {
 		Objects.requireNonNull(options, "ImportListOptions must not be null");
@@ -47,6 +72,18 @@ public class ImportListCommand extends AbstractJobCommand<ImportListResponse, Im
 		addChildNode(fileEncoding, currentNode);
 	}
 
+	/**
+	 * Reads ImportList API response into
+	 * {@link com.github.ka4ok85.wca.response.ImportListResponse}
+	 * 
+	 * @param jobPollingContainer
+	 *            - raw POJO response for Schedule a Job API
+	 * @param jobResponse
+	 *            - raw POJO response for ImportList API
+	 * @param options
+	 *            - settings for API call
+	 * @return POJO Import List Response
+	 */
 	@Override
 	public ResponseContainer<ImportListResponse> readResponse(JobPollingContainer jobPollingContainer,
 			JobResponse jobResponse, ImportListOptions options) {
