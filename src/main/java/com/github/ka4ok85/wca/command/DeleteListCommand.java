39ab63112125159b82b185bf0b2a71023810af53
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-06-10 14:36:43 -0400

    added Javadoc for DeleteListCommand

--- src/main/java/com/github/ka4ok85/wca/command/DeleteListCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/DeleteListCommand.java
@@ -13,6 +13,23 @@ import com.github.ka4ok85.wca.response.JobResponse;
 import com.github.ka4ok85.wca.response.ResponseContainer;
 import com.github.ka4ok85.wca.response.containers.JobPollingContainer;
 
+/**
+ * <strong>Class for interacting with WCA DeleteList API.</strong> It builds XML
+ * request for DeleteList API using
+ * {@link com.github.ka4ok85.wca.options.DeleteListOptions} and reads response
+ * into {@link com.github.ka4ok85.wca.response.DeleteListResponse}.
+ * <p>
+ * It relies on Spring's {@link org.springframework.web.client.RestTemplate} for
+ * synchronous client-side HTTP access.
+ * </p>
+ * <p>
+ * It relies on parent {@link com.github.ka4ok85.wca.command.AbstractJobCommand}
+ * for polling mechanism.
+ * </p>
+ *
+ * @author Evgeny Makovetsky
+ * @since 0.0.2
+ */
 @Service
 @Scope("prototype")
 public class DeleteListCommand extends AbstractJobCommand<DeleteListResponse, DeleteListOptions> {
@@ -22,6 +39,14 @@ public class DeleteListCommand extends AbstractJobCommand<DeleteListResponse, De
 	@Autowired
 	private DeleteListResponse deleteListResponse;
 
+	/**
+	 * Builds XML request for DeleteList API using
+	 * {@link com.github.ka4ok85.wca.options.DeleteListOptions}
+	 * 
+	 * @param options
+	 *            - settings for API call
+	 * @return void
+	 */
 	@Override
 	public void buildXmlRequest(DeleteListOptions options) {
 		Objects.requireNonNull(options, "DeleteListOptions must not be null");
@@ -47,6 +72,18 @@ public class DeleteListCommand extends AbstractJobCommand<DeleteListResponse, De
 		addBooleanParameter(methodElement, "RECURSIVE", options.isRecursive());
 	}
 
+	/**
+	 * Reads DeleteList API response into
+	 * {@link com.github.ka4ok85.wca.response.DeleteListResponse}
+	 * 
+	 * @param jobPollingContainer
+	 *            - raw POJO response for Schedule a Job API
+	 * @param jobResponse
+	 *            - raw POJO response for DeleteList API
+	 * @param options
+	 *            - settings for API call
+	 * @return POJO Delete List Response
+	 */
 	@Override
 	public ResponseContainer<DeleteListResponse> readResponse(JobPollingContainer jobPollingContainer,
 			JobResponse jobResponse, DeleteListOptions options) {
