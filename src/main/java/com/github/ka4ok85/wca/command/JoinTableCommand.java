8c05b4a5cd429f28a56d380595f8239fe212c486
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-06-11 16:33:44 -0400

    added Javadoc for JoinTable command

--- src/main/java/com/github/ka4ok85/wca/command/JoinTableCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/JoinTableCommand.java
@@ -15,6 +15,23 @@ import com.github.ka4ok85.wca.response.JoinTableResponse;
 import com.github.ka4ok85.wca.response.ResponseContainer;
 import com.github.ka4ok85.wca.response.containers.JobPollingContainer;
 
+/**
+ * <strong>Class for interacting with WCA JoinTable API.</strong> It builds XML
+ * request for JoinTable API using
+ * {@link com.github.ka4ok85.wca.options.JoinTableOptions} and reads response
+ * into {@link com.github.ka4ok85.wca.response.JoinTableResponse}.
+ * <p>
+ * It relies on Spring's {@link org.springframework.web.client.RestTemplate} for
+ * synchronous client-side HTTP access.
+ * </p>
+ * <p>
+ * It relies on parent {@link com.github.ka4ok85.wca.command.AbstractJobCommand}
+ * for polling mechanism.
+ * </p>
+ *
+ * @author Evgeny Makovetsky
+ * @since 0.0.2
+ */
 @Service
 @Scope("prototype")
 public class JoinTableCommand extends AbstractJobCommand<JoinTableResponse, JoinTableOptions> {
@@ -24,6 +41,14 @@ public class JoinTableCommand extends AbstractJobCommand<JoinTableResponse, Join
 	@Autowired
 	private JoinTableResponse joinTableResponse;
 
+	/**
+	 * Builds XML request for JoinTable API using
+	 * {@link com.github.ka4ok85.wca.options.JoinTableOptions}
+	 * 
+	 * @param options
+	 *            - settings for API call
+	 * @return void
+	 */
 	@Override
 	public void buildXmlRequest(JoinTableOptions options) {
 		Objects.requireNonNull(options, "JoinTableOptions must not be null");
@@ -87,6 +112,18 @@ public class JoinTableCommand extends AbstractJobCommand<JoinTableResponse, Join
 		}
 	}
 
+	/**
+	 * Reads JoinTable API response into
+	 * {@link com.github.ka4ok85.wca.response.JoinTableResponse}
+	 * 
+	 * @param jobPollingContainer
+	 *            - raw POJO response for Schedule a Job API
+	 * @param jobResponse
+	 *            - raw POJO response for JoinTable API
+	 * @param options
+	 *            - settings for API call
+	 * @return POJO Join Table Response
+	 */
 	@Override
 	public ResponseContainer<JoinTableResponse> readResponse(JobPollingContainer jobPollingContainer,
 			JobResponse jobResponse, JoinTableOptions options) {
