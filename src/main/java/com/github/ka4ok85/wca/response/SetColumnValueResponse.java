f61a34e6827c21d1dd4b66f9bc1166ee8a8dac06
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-07-08 17:07:39 -0400

    added SetColumnValueResponse class

---
+++ src/main/java/com/github/ka4ok85/wca/response/SetColumnValueResponse.java
@@ -0,0 +1,43 @@
+package com.github.ka4ok85.wca.response;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class SetColumnValueResponse extends AbstractResponse {
+	private Long jobId;
+	private String description;
+	private String listName;
+
+	public Long getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(Long jobId) {
+		this.jobId = jobId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getListName() {
+		return listName;
+	}
+
+	public void setListName(String listName) {
+		this.listName = listName;
+	}
+
+	@Override
+	public String toString() {
+		return "SetColumnValueResponse [jobId=" + jobId + ", description=" + description + ", listName=" + listName
+				+ "]";
+	}
+
+}
