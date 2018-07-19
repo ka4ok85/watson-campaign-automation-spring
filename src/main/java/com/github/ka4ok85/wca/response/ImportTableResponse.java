b33d36bc4a52d29101d0257d45c0c057cd4d5d89
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-07-19 15:40:09 -0400

    added ImportTableResponse class

---
+++ src/main/java/com/github/ka4ok85/wca/response/ImportTableResponse.java
@@ -0,0 +1,33 @@
+package com.github.ka4ok85.wca.response;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class ImportTableResponse extends AbstractResponse {
+	private Long jobId;
+	private String description;
+
+	public Long getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(Long jobId) {
+		this.jobId = jobId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "ImportTableResponse [jobId=" + jobId + ", description=" + description + "]";
+	}
+
+}
