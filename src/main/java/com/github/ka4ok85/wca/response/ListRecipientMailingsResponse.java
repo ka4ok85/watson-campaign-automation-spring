e38f62fc20b6be8abefc28a4cdbc6f6bde403d35
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-07-23 09:21:48 -0400

    added ListRecipientMailingsResponse class

---
+++ src/main/java/com/github/ka4ok85/wca/response/ListRecipientMailingsResponse.java
@@ -0,0 +1,29 @@
+package com.github.ka4ok85.wca.response;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.github.ka4ok85.wca.response.containers.RecipientMailing;
+
+@Component
+@Scope("prototype")
+public class ListRecipientMailingsResponse extends AbstractResponse {
+	private List<RecipientMailing> mailings = new ArrayList<RecipientMailing>();
+
+	public List<RecipientMailing> getMailings() {
+		return mailings;
+	}
+
+	public void setMailings(List<RecipientMailing> mailings) {
+		this.mailings = mailings;
+	}
+
+	@Override
+	public String toString() {
+		return "ListRecipientMailingsResponse []";
+	}
+
+}
