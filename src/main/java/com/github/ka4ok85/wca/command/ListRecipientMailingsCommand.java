28ad8541fbd1d8277040113ff460c786a112fa4b
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-07-23 11:15:13 -0400

    added logic for ListRecipientMailings API

--- src/main/java/com/github/ka4ok85/wca/command/ListRecipientMailingsCommand.java
+++ src/main/java/com/github/ka4ok85/wca/command/ListRecipientMailingsCommand.java
@@ -1,24 +1,36 @@
 package com.github.ka4ok85.wca.command;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
-import java.util.Map.Entry;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
-import org.w3c.dom.CDATASection;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import com.github.ka4ok85.wca.exceptions.EngageApiException;
+import com.github.ka4ok85.wca.exceptions.JobBadStateException;
 import com.github.ka4ok85.wca.options.ListRecipientMailingsOptions;
 import com.github.ka4ok85.wca.response.ListRecipientMailingsResponse;
 import com.github.ka4ok85.wca.response.ResponseContainer;
+import com.github.ka4ok85.wca.response.containers.RecipientMailing;
 
 /**
- * <strong>Class for interacting with WCA ListRecipientMailings API.</strong> It builds
- * XML request for ListRecipientMailings API using
- * {@link com.github.ka4ok85.wca.options.ListRecipientMailingsOptions} and reads response
- * into {@link com.github.ka4ok85.wca.response.ListRecipientMailingsResponse}.
+ * <strong>Class for interacting with WCA ListRecipientMailings API.</strong> It
+ * builds XML request for ListRecipientMailings API using
+ * {@link com.github.ka4ok85.wca.options.ListRecipientMailingsOptions} and reads
+ * response into
+ * {@link com.github.ka4ok85.wca.response.ListRecipientMailingsResponse}.
  * <p>
  * It relies on Spring's {@link org.springframework.web.client.RestTemplate} for
  * synchronous client-side HTTP access.
@@ -29,7 +41,8 @@ import com.github.ka4ok85.wca.response.ResponseContainer;
  */
 @Service
 @Scope("prototype")
-public class ListRecipientMailingsCommand  extends AbstractInstantCommand<ListRecipientMailingsResponse, ListRecipientMailingsOptions> {
+public class ListRecipientMailingsCommand
+		extends AbstractInstantCommand<ListRecipientMailingsResponse, ListRecipientMailingsOptions> {
 
 	private static final String apiMethodName = "ListRecipientMailings";
 
@@ -45,7 +58,18 @@ public class ListRecipientMailingsCommand  extends AbstractInstantCommand<ListRe
 	 */
 	@Override
 	public void buildXmlRequest(ListRecipientMailingsOptions options) {
+		Objects.requireNonNull(options, "ListRecipientMailingsOptions must not be null");
 
+		Element methodElement = doc.createElement(apiMethodName);
+		currentNode = addChildNode(methodElement, null);
+
+		Element listID = doc.createElement("LIST_ID");
+		listID.setTextContent(options.getListId().toString());
+		addChildNode(listID, currentNode);
+
+		Element recipientID = doc.createElement("RECIPIENT_ID");
+		recipientID.setTextContent(options.getRecipientId().toString());
+		addChildNode(recipientID, currentNode);
 	}
 
 	/**
@@ -59,8 +83,61 @@ public class ListRecipientMailingsCommand  extends AbstractInstantCommand<ListRe
 	 * @return POJO ListRecipientMailingsResponse
 	 */
 	@Override
-	public ResponseContainer<ListRecipientMailingsResponse> readResponse(Node resultNode, ListRecipientMailingsOptions options) {
-		return null;
+	public ResponseContainer<ListRecipientMailingsResponse> readResponse(Node resultNode,
+			ListRecipientMailingsOptions options) {
+		XPathFactory factory = XPathFactory.newInstance();
+		XPath xpath = factory.newXPath();
+
+		List<RecipientMailing> mailings = new ArrayList<RecipientMailing>();
+		final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yy K:mm a");
+		try {
+			NodeList mailingsNode = (NodeList) xpath.evaluate("Mailing", resultNode, XPathConstants.NODESET);
+			Node mailingNode;
+			for (int i = 0; i < mailingsNode.getLength(); i++) {
+				RecipientMailing mailing = new RecipientMailing();
+				mailingNode = mailingsNode.item(i);
+
+				mailing.setMailingId(Long.parseLong(
+						((Node) xpath.evaluate("MailingId", mailingNode, XPathConstants.NODE)).getTextContent()));
+				mailing.setMailingName(
+						((Node) xpath.evaluate("MailingName ", mailingNode, XPathConstants.NODE)).getTextContent());
+				mailing.setSentDateTime(LocalDateTime.parse(
+						((Node) xpath.evaluate("SentTS", mailingNode, XPathConstants.NODE)).getTextContent(),
+						formatter));
+				mailing.setTotalAttachments(
+						Long.parseLong(((Node) xpath.evaluate("TotalAttachments", mailingNode, XPathConstants.NODE))
+								.getTextContent()));
+				mailing.setTotalBounces(Long.parseLong(
+						((Node) xpath.evaluate("TotalBounces", mailingNode, XPathConstants.NODE)).getTextContent()));
+				mailing.setTotalClicks(Long.parseLong(
+						((Node) xpath.evaluate("TotalClicks", mailingNode, XPathConstants.NODE)).getTextContent()));
+				mailing.setTotalClickstreams(
+						Long.parseLong(((Node) xpath.evaluate("TotalClickstreams", mailingNode, XPathConstants.NODE))
+								.getTextContent()));
+				mailing.setTotalConversions(
+						Long.parseLong(((Node) xpath.evaluate("TotalConversions", mailingNode, XPathConstants.NODE))
+								.getTextContent()));
+				mailing.setTotalForwards(Long.parseLong(
+						((Node) xpath.evaluate("TotalForwards", mailingNode, XPathConstants.NODE)).getTextContent()));
+				mailing.setTotalMediaPlays(Long.parseLong(
+						((Node) xpath.evaluate("TotalMediaPlays", mailingNode, XPathConstants.NODE)).getTextContent()));
+				mailing.setTotalOpens(Long.parseLong(
+						((Node) xpath.evaluate("TotalOpens", mailingNode, XPathConstants.NODE)).getTextContent()));
+				mailing.setTotalOptOuts(Long.parseLong(
+						((Node) xpath.evaluate("TotalOptOuts", mailingNode, XPathConstants.NODE)).getTextContent()));
+
+				mailings.add(mailing);
+			}
+
+			listRecipientMailingsResponse.setMailings(mailings);
+		} catch (XPathExpressionException | JobBadStateException e) {
+			throw new EngageApiException(e.getMessage());
+		}
+
+		ResponseContainer<ListRecipientMailingsResponse> response = new ResponseContainer<ListRecipientMailingsResponse>(
+				listRecipientMailingsResponse);
+
+		return response;
 	}
 
 }
