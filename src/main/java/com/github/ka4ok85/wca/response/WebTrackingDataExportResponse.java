72fb5a6cbfb393f39939511b11ab16e92ff965ad
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-07-26 13:38:35 -0400

    added WebTrackingDataExportResponse class

---
+++ src/main/java/com/github/ka4ok85/wca/response/WebTrackingDataExportResponse.java
@@ -0,0 +1,34 @@
+package com.github.ka4ok85.wca.response;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class WebTrackingDataExportResponse extends AbstractResponse {
+
+	private Long jobId;
+	private String description;
+
+	public Long getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(Long jobId) {
+		this.jobId = jobId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "WebTrackingDataExportResponse [jobId=" + jobId + ", description=" + description + "]";
+	}
+
+}
