d46542f260411d2268ffdf6450129ee26d74f2dd
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-08-07 17:22:06 -0400

    added more unit tests for Engage service

--- src/test/java/com/github/ka4ok85/wca/EngageTest.java
+++ src/test/java/com/github/ka4ok85/wca/EngageTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -11,11 +14,14 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.github.ka4ok85.wca.command.AddRecipientCommand;
 import com.github.ka4ok85.wca.command.CreateContactListCommand;
+import com.github.ka4ok85.wca.command.CreateTableCommand;
 import com.github.ka4ok85.wca.command.DeleteListCommand;
 import com.github.ka4ok85.wca.command.DoubleOptInRecipientCommand;
 import com.github.ka4ok85.wca.command.ExportListCommand;
 import com.github.ka4ok85.wca.command.ExportTableCommand;
 import com.github.ka4ok85.wca.command.GetFolderPathCommand;
+import com.github.ka4ok85.wca.command.GetListsCommand;
+import com.github.ka4ok85.wca.command.JoinTableCommand;
 import com.github.ka4ok85.wca.command.OptOutRecipientCommand;
 import com.github.ka4ok85.wca.command.RemoveRecipientCommand;
 import com.github.ka4ok85.wca.command.SelectRecipientDataCommand;
@@ -26,22 +32,28 @@ import com.github.ka4ok85.wca.constants.Visibility;
 import com.github.ka4ok85.wca.oauth.OAuthClient;
 import com.github.ka4ok85.wca.options.AddRecipientOptions;
 import com.github.ka4ok85.wca.options.CreateContactListOptions;
+import com.github.ka4ok85.wca.options.CreateTableOptions;
 import com.github.ka4ok85.wca.options.DeleteListOptions;
 import com.github.ka4ok85.wca.options.DoubleOptInRecipientOptions;
 import com.github.ka4ok85.wca.options.ExportListOptions;
 import com.github.ka4ok85.wca.options.ExportTableOptions;
 import com.github.ka4ok85.wca.options.GetFolderPathOptions;
+import com.github.ka4ok85.wca.options.GetListsOptions;
+import com.github.ka4ok85.wca.options.JoinTableOptions;
 import com.github.ka4ok85.wca.options.OptOutRecipientOptions;
 import com.github.ka4ok85.wca.options.RemoveRecipientOptions;
 import com.github.ka4ok85.wca.options.SelectRecipientDataOptions;
 import com.github.ka4ok85.wca.options.UpdateRecipientOptions;
 import com.github.ka4ok85.wca.response.AddRecipientResponse;
 import com.github.ka4ok85.wca.response.CreateContactListResponse;
+import com.github.ka4ok85.wca.response.CreateTableResponse;
 import com.github.ka4ok85.wca.response.DeleteListResponse;
 import com.github.ka4ok85.wca.response.DoubleOptInRecipientResponse;
 import com.github.ka4ok85.wca.response.ExportListResponse;
 import com.github.ka4ok85.wca.response.ExportTableResponse;
 import com.github.ka4ok85.wca.response.GetFolderPathResponse;
+import com.github.ka4ok85.wca.response.GetListsResponse;
+import com.github.ka4ok85.wca.response.JoinTableResponse;
 import com.github.ka4ok85.wca.response.OptOutRecipientResponse;
 import com.github.ka4ok85.wca.response.RemoveRecipientResponse;
 import com.github.ka4ok85.wca.response.ResponseContainer;
@@ -288,6 +300,72 @@ public class EngageTest {
 
 		assertEquals(responseContainer.getClass(), ResponseContainer.class);
 		assertEquals(responseContainer.getResposne().getClass(), RemoveRecipientResponse.class);
+		assertEquals(responseContainer.getResposne(), response);
+	}
+
+	@Test
+	public void testGetLists() {
+		OAuthClient oAuthClient = mock(OAuthClient.class);
+		SFTP sftp = mock(SFTP.class);
+		Engage engage = new Engage(oAuthClient, sftp);
+
+		GetListsOptions options = new GetListsOptions();
+		GetListsResponse response = new GetListsResponse();
+
+		GetListsCommand getListsBean = mock(GetListsCommand.class);
+		when(getListsBean.executeCommand(options)).thenReturn(new ResponseContainer<GetListsResponse>(response));
+
+		engage.setGetListsBean(getListsBean);
+
+		ResponseContainer<GetListsResponse> responseContainer = engage.getLists(options);
+
+		assertEquals(responseContainer.getClass(), ResponseContainer.class);
+		assertEquals(responseContainer.getResposne().getClass(), GetListsResponse.class);
+		assertEquals(responseContainer.getResposne(), response);
+	}
+
+	@Test
+	public void testCreateTable() {
+		OAuthClient oAuthClient = mock(OAuthClient.class);
+		SFTP sftp = mock(SFTP.class);
+		Engage engage = new Engage(oAuthClient, sftp);
+
+		CreateTableOptions options = new CreateTableOptions("test");
+		CreateTableResponse response = new CreateTableResponse();
+
+		CreateTableCommand createTableBean = mock(CreateTableCommand.class);
+		when(createTableBean.executeCommand(options)).thenReturn(new ResponseContainer<CreateTableResponse>(response));
+
+		engage.setCreateTableBean(createTableBean);
+
+		ResponseContainer<CreateTableResponse> responseContainer = engage.createTable(options);
+
+		assertEquals(responseContainer.getClass(), ResponseContainer.class);
+		assertEquals(responseContainer.getResposne().getClass(), CreateTableResponse.class);
+		assertEquals(responseContainer.getResposne(), response);
+	}
+
+	@Test
+	public void testJoinTable() {
+		OAuthClient oAuthClient = mock(OAuthClient.class);
+		SFTP sftp = mock(SFTP.class);
+		Engage engage = new Engage(oAuthClient, sftp);
+
+		Map<String, String> testMap = new HashMap<String, String>();
+		testMap.put("test1", "test2");
+
+		JoinTableOptions options = new JoinTableOptions(testMap);
+		JoinTableResponse response = new JoinTableResponse();
+
+		JoinTableCommand joinTableBean = mock(JoinTableCommand.class);
+		when(joinTableBean.executeCommand(options)).thenReturn(new ResponseContainer<JoinTableResponse>(response));
+
+		engage.setJoinTableBean(joinTableBean);
+
+		ResponseContainer<JoinTableResponse> responseContainer = engage.joinTable(options);
+
+		assertEquals(responseContainer.getClass(), ResponseContainer.class);
+		assertEquals(responseContainer.getResposne().getClass(), JoinTableResponse.class);
 		assertEquals(responseContainer.getResposne(), response);
 	}
 
